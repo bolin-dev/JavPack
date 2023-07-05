c42c469df94f6ed9224391db3911c2f389c2bf50
bolinc <bolinchan2046@gmail.com>
2023-07-05 14:05:05 +0800

    refactor: update layout

--- scripts/layout/JavDB.layout.user.js
+++ scripts/layout/JavDB.layout.user.js
@@ -24,12 +24,12 @@ GM_addStyle(`
 }
 @media (min-width: 576px) {
   .container {
-      max-width:540px;
+    max-width: 540px;
   }
 }
 @media (min-width: 768px) {
   .container {
-      max-width:720px;
+    max-width: 720px;
   }
   .movie-list.v {
     grid-template-columns: repeat(2, minmax(0, 1fr));
@@ -37,7 +37,7 @@ GM_addStyle(`
 }
 @media (min-width: 992px) {
   .container {
-      max-width:960px;
+    max-width: 960px;
   }
   .movie-list {
     grid-template-columns: repeat(2, minmax(0, 1fr));
@@ -48,12 +48,12 @@ GM_addStyle(`
 }
 @media (min-width: 1200px) {
   .container {
-      max-width:1140px !important;
+    max-width: 1140px !important;
   }
 }
 @media (min-width: 1400px) {
   .container {
-      max-width:1320px !important;
+    max-width: 1320px !important;
   }
   .movie-list {
     grid-template-columns: repeat(3, minmax(0, 1fr));
