579a2fa5406a20cadcc53852bf264d8fba0e4491
bolinc <bolinchan2046@gmail.com>
2023-07-05 14:01:17 +0800

    refactor: update style & layout

--- scripts/layout/JavDB.layout.user.js
+++ scripts/layout/JavDB.layout.user.js
@@ -14,54 +14,52 @@
 // @compatible      edge
 // ==/UserScript==
 
-(function () {
-  GM_addStyle(`
-  .movie-list {
-    grid-gap: 1rem !important;
-    grid-template-columns: repeat(1, minmax(0, 1fr));
+GM_addStyle(`
+.movie-list {
+  grid-gap: 1rem !important;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+}
+.movie-list.v {
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+}
+@media (min-width: 576px) {
+  .container {
+      max-width:540px;
+  }
+}
+@media (min-width: 768px) {
+  .container {
+      max-width:720px;
   }
   .movie-list.v {
-    grid-template-columns: repeat(1, minmax(0, 1fr));
+    grid-template-columns: repeat(2, minmax(0, 1fr));
   }
-  @media (min-width: 576px) {
-    .container {
-        max-width:540px;
-    }
+}
+@media (min-width: 992px) {
+  .container {
+      max-width:960px;
   }
-  @media (min-width: 768px) {
-    .container {
-        max-width:720px;
-    }
-    .movie-list.v {
-      grid-template-columns: repeat(2, minmax(0, 1fr));
-    }
+  .movie-list {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
   }
-  @media (min-width: 992px) {
-    .container {
-        max-width:960px;
-    }
-    .movie-list {
-      grid-template-columns: repeat(2, minmax(0, 1fr));
-    }
-    .movie-list.v {
-      grid-template-columns: repeat(3, minmax(0, 1fr));
-    }
+  .movie-list.v {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
   }
-  @media (min-width: 1200px) {
-    .container {
-        max-width:1140px !important;
-    }
+}
+@media (min-width: 1200px) {
+  .container {
+      max-width:1140px !important;
   }
-  @media (min-width: 1400px) {
-    .container {
-        max-width:1320px !important;
-    }
-    .movie-list {
-      grid-template-columns: repeat(3, minmax(0, 1fr));
-    }
-    .movie-list.v {
-      grid-template-columns: repeat(4, minmax(0, 1fr));
-    }
+}
+@media (min-width: 1400px) {
+  .container {
+      max-width:1320px !important;
   }
-  `);
-})();
+  .movie-list {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+  .movie-list.v {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+}
+`);
