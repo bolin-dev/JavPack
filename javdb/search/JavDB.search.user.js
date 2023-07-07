b61cc96ae0dd4048724a532fe2b3c469931bbbe0
bolinc <bolinchan2046@gmail.com>
2023-07-08 00:07:16 +0800

    feat: add search

---
+++ javdb/search/JavDB.search.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name            JavDB.search
+// @namespace       JavDB.search@blc
+// @version         0.0.1
+// @author          blc
+// @description     快捷搜索
+// @include         /^https:\/\/javdb\d*\.com\/.*$/
+// @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
+// @require         https://greasyfork.org/scripts/470274-javpack-listener/code/JavPacklistener.js?version=1216236
+// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
+// @run-at          document-start
+// @grant           GM_openInTab
+// @license         GPL-3.0-only
+// @compatible      chrome
+// @compatible      edge
+// ==/UserScript==
+
+(function () {
+  const focusSearch = e => {
+    if (["INPUT", "TEXTAREA"].includes(document.activeElement.nodeName)) return;
+    if (e.key === "/") document.querySelector("#video-search")?.focus();
+  };
+
+  const search = async e => {
+    if (e.ctrlKey && e.key === "/") {
+      const text = (await navigator.clipboard.readText())?.trim();
+      if (!text) return;
+
+      GM_openInTab(`${location.origin}/search?q=%s`.replaceAll("%s", text), {
+        active: true,
+        setParent: true,
+      });
+    }
+  };
+
+  autoListener([
+    {
+      path: "_global",
+      events: {
+        keyup: focusSearch,
+        keydown: search,
+      },
+    },
+  ]);
+})();
