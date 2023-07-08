8a7361b51874c79d48f5603f4942654da8e155d6
bolinc <bolinchan2046@gmail.com>
2023-07-09 04:20:50 +0800

    feat: update filter

--- javdb/filter/JavDB.filter.user.js
+++ javdb/filter/JavDB.filter.user.js
@@ -15,23 +15,53 @@
 // ==/UserScript==
 
 (function () {
+  const hiddenList = [];
+  const highlightList = [];
+  if (!hiddenList.length && !highlightList.length) return;
+
   const container = document.querySelector(".movie-list");
   if (!container) return;
 
   const childList = container.querySelectorAll(".item");
   if (!childList?.length) return;
 
+  const parseRes = node => {
+    const title = node.querySelector(".video-title");
+    const code = title.querySelector("strong").textContent;
+    const name = title.textContent.replace(code, "").trim();
+    const score = node
+      .querySelector(".score .value")
+      .textContent.replace(/\u00A0/g, "")
+      .trim();
+    const date = node.querySelector(".meta").textContent.trim();
+    const tags = [...node.querySelectorAll(".tags .tag")].map(tag => tag.textContent);
+    return `[${code}][${name}][${score}][${date}][${tags}]`;
+  };
+
   const filter = nodeList => {
-    console.log(nodeList);
+    for (const node of nodeList) {
+      const res = parseRes(node);
+
+      if (hiddenList.some(item => item.test(res))) {
+        node.classList.add("is-hidden");
+        continue;
+      }
+      if (highlightList.some(item => item.test(res))) node.classList.add("is-highlight");
+    }
   };
   filter(childList);
 
-  const mutationObserver = new MutationObserver((mutationsList, observer) => {
+  const callback = (mutationsList, observer) => {
     for (let { type, addedNodes } of mutationsList) {
-      if (type === "childList" && addedNodes?.length) filter(addedNodes);
+      if (type !== "childList") continue;
+      if (!addedNodes?.length) continue;
+      if (addedNodes.length < 40) observer.disconnect();
+      filter(addedNodes);
     }
-  });
+  };
+
+  const mutationObserver = new MutationObserver(callback);
   mutationObserver.observe(container, { childList: true, attributes: false });
 
-  GM_addStyle(`.hightlight {}`);
+  GM_addStyle(".is-highlight{box-shadow:0 0 0 4px red!important}");
 })();
