0d767920f0b652d628a29f0fe61621fa48ca4e99
bolinc <bolinchan2046@gmail.com>
2023-07-09 03:23:29 +0800

    refactor: update scroll

--- javdb/scroll/JavDB.scroll.user.js
+++ javdb/scroll/JavDB.scroll.user.js
@@ -21,7 +21,6 @@
   const selectors = {
     container: ":is(.movie-list, .actors, .section-container)",
     pagination: "nav.pagination .pagination-next",
-    child: ":is(div, a)",
   };
 
   const container = document.querySelector(selectors.container);
@@ -35,13 +34,14 @@
   loading.textContent = "加载中...";
   container.insertAdjacentElement("afterend", loading);
 
+  let isLoading = false;
+
   const queryNext = dom => {
-    const list = dom.querySelectorAll(`${selectors.container} > ${selectors.child}`);
     const url = dom.querySelector(selectors.pagination)?.href;
+    const list = dom.querySelectorAll(`${selectors.container} > :is(div, a)`);
     return { url, list };
   };
 
-  let isLoading = false;
   const callback = async (entries, observer) => {
     if (!entries[0].isIntersecting || isLoading) return;
 
@@ -63,74 +63,27 @@
   intersectionObserver.observe(loading);
 
   GM_addStyle(`
-  .actors {
-    --xs-item-height: 580px;
-    --sm-item-height: 299px;
-    --md-item-height: 266px;
-    --lg-item-height: 265px;
-    --xl-item-height: 252px;
-    --xxl-item-height: 244px;
+  .actors{--xs-item-height:580px;--sm-item-height:299px;--md-item-height:266px;--lg-item-height:265px;--xl-item-height:252px;--xxl-item-height:244px}
+  .movie-list{--xs-item-height:470px;--sm-item-height:468px;--md-item-height:589px;--lg-item-height:423px;--xl-item-height:483px;--xxl-item-height:394px}
+  .movie-list.v{--xs-item-height:868px;--sm-item-height:864px;--md-item-height:606px;--lg-item-height:548px;--xl-item-height:630px;--xxl-item-height:560px}
+  .section-container{--xs-item-height:55px;--sm-item-height:55px;--md-item-height:55px;--lg-item-height:55px;--xl-item-height:55px;--xxl-item-height:55px}
+  :is(.movie-list,.actors,.section-container)>:is(div,a){content-visibility:auto;contain-intrinsic-size:auto var(--xs-item-height)}
+  @media (min-width:576px){
+  :is(.movie-list,.actors,.section-container)>:is(div,a){contain-intrinsic-size:auto var(--sm-item-height)}
   }
-  .movie-list {
-    --xs-item-height: 470px;
-    --sm-item-height: 468px;
-    --md-item-height: 589px;
-    --lg-item-height: 423px;
-    --xl-item-height: 483px;
-    --xxl-item-height: 394px;
+  @media (min-width:768px){
+  :is(.movie-list,.actors,.section-container)>:is(div,a){contain-intrinsic-size:auto var(--md-item-height)}
   }
-  .movie-list.v {
-    --xs-item-height: 868px;
-    --sm-item-height: 864px;
-    --md-item-height: 606px;
-    --lg-item-height: 548px;
-    --xl-item-height: 630px;
-    --xxl-item-height: 560px;
+  @media (min-width:992px){
+  :is(.movie-list,.actors,.section-container)>:is(div,a){contain-intrinsic-size:auto var(--lg-item-height)}
   }
-  .section-container {
-    --xs-item-height: 55px;
-    --sm-item-height: 55px;
-    --md-item-height: 55px;
-    --lg-item-height: 55px;
-    --xl-item-height: 55px;
-    --xxl-item-height: 55px;
+  @media (min-width:1200px){
+  :is(.movie-list,.actors,.section-container)>:is(div,a){contain-intrinsic-size:auto var(--xl-item-height)}
   }
-  :is(.movie-list, .actors, .section-container) > :is(div, a) {
-    content-visibility: auto;
-    contain-intrinsic-size: auto var(--xs-item-height);
+  @media (min-width:1400px){
+  :is(.movie-list,.actors,.section-container)>:is(div,a){contain-intrinsic-size:auto var(--xxl-item-height)}
   }
-  @media (min-width: 576px) {
-    :is(.movie-list, .actors, .section-container) > :is(div, a) {
-      contain-intrinsic-size: auto var(--sm-item-height);
-    }
-  }
-  @media (min-width: 768px) {
-    :is(.movie-list, .actors, .section-container) > :is(div, a) {
-      contain-intrinsic-size: auto var(--md-item-height);
-    }
-  }
-  @media (min-width: 992px) {
-    :is(.movie-list, .actors, .section-container) > :is(div, a) {
-      contain-intrinsic-size: auto var(--lg-item-height);
-    }
-  }
-  @media (min-width: 1200px) {
-    :is(.movie-list, .actors, .section-container) > :is(div, a) {
-      contain-intrinsic-size: auto var(--xl-item-height);
-    }
-  }
-  @media (min-width: 1400px) {
-    :is(.movie-list, .actors, .section-container) > :is(div, a) {
-      contain-intrinsic-size: auto var(--xxl-item-height);
-    }
-  }
-  nav.pagination {
-    display: none;
-  }
-  .pagination-loading {
-    font-size: 14px;
-    padding-top: 1rem;
-    text-align: center;
-  }
+  nav.pagination{display:none}
+  .pagination-loading{font-size:14px;padding-top:1rem;text-align:center}
   `);
 })();
