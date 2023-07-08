df729dff31f8db29149e4b119373a9709ba7a8e8
bolinc <bolinchan2046@gmail.com>
2023-07-09 03:12:07 +0800

    refactor: update nav

--- javdb/nav/JavDB.nav.user.js
+++ javdb/nav/JavDB.nav.user.js
@@ -6,7 +6,6 @@
 // @description     快捷导航
 // @include         /^https:\/\/javdb\d*\.com\/(?!v\/).*$/
 // @icon            https://raw.githubusercontent.com/bolin-dev/JavPack/main/static/logo.png
-// @require         https://raw.githubusercontent.com/bolin-dev/JavPack/main/libs/listener/JavPack.listener.lib.js
 // @supportURL      https://t.me/+bAWrOoIqs3xmMjll
 // @run-at          document-start
 // @license         GPL-3.0-only
@@ -24,16 +23,8 @@
     e.stopImmediatePropagation();
 
     document
-      .querySelector(`.pagination .pagination-${e.key === "ArrowLeft" ? "previous" : "next"}`)
+      .querySelector(`nav.pagination .pagination-${e.key === "ArrowLeft" ? "previous" : "next"}`)
       ?.click();
   };
-
-  autoListener([
-    {
-      path: "_global",
-      events: {
-        keyup: navigation,
-      },
-    },
-  ]);
+  document.addEventListener("keyup", navigation);
 })();
