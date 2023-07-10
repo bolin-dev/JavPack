595ee915b4b0c73cfadf65a8555a213677655e0c
bolinc <bolinchan2046@gmail.com>
2023-07-10 12:12:14 +0800

    feat: update trailer lib

--- libs/trailer/JavPack.trailer.lib.js
+++ libs/trailer/JavPack.trailer.lib.js
@@ -1,46 +1,3 @@
-async function fetchMsin(code) {
-  const dom = await request(`https://db.msin.jp/branch/search?sort=jp.movie&str=${code}`);
-  if (!dom || dom.querySelector("#center_main .error_string")) return;
-
-  let cid = dom.querySelector(".mv_fileName")?.textContent;
-  if (cid) return verifyFanzaCid(cid);
-
-  dom.querySelectorAll("#bottom_content .movie_view_jp .movie_info").forEach(item => {
-    const href = item.querySelector("a.playbutton")?.getAttribute("href");
-    if (href?.includes("sample.fanza?id=")) {
-      cid = href.split("=").at(-1);
-      return;
-    }
-  });
-  if (cid) return verifyFanzaCid(cid);
-}
-
-async function verifyFanzaCid(cid) {
-  const HOST = "https://cc3001.dmm.co.jp";
-  const FIRST = cid[0];
-  const SECOND = cid.substring(0, 3);
-
-  let trailer = "";
-  const list = [
-    `${HOST}/litevideo/freepv/${FIRST}/${SECOND}/${cid}/${cid}_mhb_w.mp4`,
-    `${HOST}/litevideo/freepv/${FIRST}/${SECOND}/${cid}/${cid}_dmb_w.mp4`,
-    `${HOST}/litevideo/freepv/${FIRST}/${SECOND}/${cid}/${cid}_dm_w.mp4`,
-    `${HOST}/litevideo/freepv/${FIRST}/${SECOND}/${cid}/${cid}_sm_w.mp4`,
-    `${HOST}/vrsample/${FIRST}/${SECOND}/${cid}/${cid}vrlite.mp4`,
-  ];
-
-  const res = await Promise.allSettled(list.map(item => request({ url: item, method: "HEAD" })));
-  for (let index = 0, { length } = res; index < length; index++) {
-    const { status, value } = res[index];
-    if (status !== "fulfilled" || !value) continue;
-
-    trailer = list[index];
-    break;
-  }
-
-  return trailer;
-}
-
 function fetchJavspyl(code) {
   return request({
     method: "POST",
@@ -48,4 +5,84 @@ function fetchJavspyl(code) {
     url: "https://v2.javspyl.tk/api/",
     headers: { origin: "https://javspyl.tk" },
   }).then(res => res?.info?.url);
+}
+
+async function fetchStudio(code, studio) {
+  const arr = studio
+    .split(",")
+    .map(item => item.trim())
+    .filter(Boolean);
+
+  const studioList = [
+    {
+      studio: "東京熱",
+      samples: [`https://my.cdn.tokyo-hot.com/media/samples/%s.mp4`],
+    },
+    {
+      studio: "カリビアンコム",
+      samples: [
+        `https://smovie.caribbeancom.com/sample/movies/%s/720p.mp4`,
+        `https://smovie.caribbeancom.com/sample/movies/%s/1080p.mp4`,
+        `https://smovie.caribbeancom.com/sample/movies/%s/480p.mp4`,
+        `https://smovie.caribbeancom.com/sample/movies/%s/360p.mp4`,
+        `https://smovie.caribbeancom.com/sample/movies/%s/240p.mp4`,
+      ],
+    },
+    {
+      studio: "一本道",
+      samples: [
+        `http://smovie.1pondo.tv/sample/movies/%s/720p.mp4`,
+        `http://smovie.1pondo.tv/sample/movies/%s/1080p.mp4`,
+        `http://smovie.1pondo.tv/sample/movies/%s/480p.mp4`,
+        `http://smovie.1pondo.tv/sample/movies/%s/360p.mp4`,
+        `http://smovie.1pondo.tv/sample/movies/%s/240p.mp4`,
+      ],
+    },
+    {
+      studio: "HEYZO",
+      parse: code => code.replaceAll("HEYZO-", ""),
+      samples: [`https://sample.heyzo.com/contents/3000/%s/heyzo_hd_%s_sample.mp4`],
+    },
+    {
+      studio: "天然むすめ",
+      samples: [
+        `https://smovie.10musume.com/sample/movies/%s/720p.mp4`,
+        `https://smovie.10musume.com/sample/movies/%s/1080p.mp4`,
+        `https://smovie.10musume.com/sample/movies/%s/480p.mp4`,
+        `https://smovie.10musume.com/sample/movies/%s/360p.mp4`,
+        `https://smovie.10musume.com/sample/movies/%s/240p.mp4`,
+      ],
+    },
+    {
+      studio: "パコパコママ",
+      samples: [
+        `https://fms.pacopacomama.com/hls/sample/pacopacomama.com/%s/720p.mp4`,
+        `https://fms.pacopacomama.com/hls/sample/pacopacomama.com/%s/1080p.mp4`,
+        `https://fms.pacopacomama.com/hls/sample/pacopacomama.com/%s/480p.mp4`,
+        `https://fms.pacopacomama.com/hls/sample/pacopacomama.com/%s/360p.mp4`,
+        `https://fms.pacopacomama.com/hls/sample/pacopacomama.com/%s/240p.mp4`,
+      ],
+    },
+  ];
+
+  let list = [];
+  for (const { samples, ...item } of studioList) {
+    if (arr.every(it => it !== item.studio)) continue;
+
+    const word = item?.parse ? item.parse(code) : code;
+    list = samples.map(url => url.replaceAll("%s", word));
+    break;
+  }
+  if (!list.length) return;
+
+  let trailer = "";
+  const res = await Promise.allSettled(list.map(url => request({ url, method: "HEAD" })));
+  for (let index = 0, { length } = res; index < length; index++) {
+    const { status, value } = res[index];
+    if (status !== "fulfilled" || !value) continue;
+
+    trailer = list[index];
+    break;
+  }
+  return trailer;
 }
