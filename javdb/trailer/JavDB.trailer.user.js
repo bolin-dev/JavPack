40f328bef6ff7eb50cdd1005841436753a8d2933
bolinc <bolinchan2046@gmail.com>
2023-07-10 12:31:07 +0800

    feat: update trailer

--- javdb/trailer/JavDB.trailer.user.js
+++ javdb/trailer/JavDB.trailer.user.js
@@ -7,8 +7,15 @@
 // @include         /^https:\/\/javdb\d*\.com\/v\/\w+/
 // @icon            https://raw.githubusercontent.com/bolin-dev/JavPack/main/static/logo.png
 // @require         https://raw.githubusercontent.com/bolin-dev/JavPack/main/libs/request/JavPack.request.lib.js
+// @require         https://raw.githubusercontent.com/bolin-dev/JavPack/main/libs/trailer/JavPack.trailer.lib.js
 // @supportURL      https://t.me/+bAWrOoIqs3xmMjll
-// @connect         *
+// @connect         caribbeancom.com
+// @connect         pacopacomama.com
+// @connect         tokyo-hot.com
+// @connect         10musume.com
+// @connect         javspyl.tk
+// @connect         1pondo.tv
+// @connect         heyzo.com
 // @run-at          document-end
 // @grant           GM_xmlhttpRequest
 // @grant           GM_addStyle
@@ -17,7 +24,7 @@
 // @compatible      edge
 // ==/UserScript==
 
-(function () {
+(async function () {
   const mid = location.pathname.split("/").at(-1);
   if (!mid) return;
 
@@ -30,11 +37,25 @@
     const code = infoNode.querySelector(".first-block .value")?.textContent;
     if (!code) return;
 
-    const title = document.querySelector(".title.is-4");
-    if (title.querySelector("strong").textContent.includes("無碼")) {
-      console.log("无码");
-    } else {
-      console.log("有码");
+    if (document.querySelector(".title.is-4 strong").textContent.includes("無碼")) {
+      let studio = "";
+      infoNode.querySelectorAll(".panel-block").forEach(node => {
+        if (node.querySelector("strong")?.textContent === "片商:") {
+          studio = node.querySelector(".value").textContent;
+          return;
+        }
+      });
+
+      if (studio) trailer = await fetchStudio(code, studio);
     }
+
+    if (!trailer) trailer = await fetchJavspyl(code);
   }
+
+  if (!trailer || /\.m3u8?$/i.test(trailer)) return;
+
+  if (!trailer.includes("//")) trailer = `https://${trailer}`;
+  localStorage.setItem(mid, trailer);
+
+  console.log(trailer);
 })();
