c2c0df2f68344ef454e19813fe401efad8e0dbe1
blc <bolinchan2046@gmail.com>
2023-09-08 18:48:55 +0800

    refactor: 更新文档

---
+++ drive/delete/Drive.delete.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name            Drive.delete
+// @namespace       Drive.delete@blc
+// @version         0.0.1
+// @author          blc
+// @description     播放页删除
+// @include         /^https:\/\/v\.anxia\.com\/\?pickcode=\w+$/
+// @icon            https://raw.githubusercontent.com/bolin-dev/JavPack/main/static/logo.png
+// @require         https://raw.githubusercontent.com/bolin-dev/JavPack/main/libs/request/JavPack.request.lib.js
+// @require         https://raw.githubusercontent.com/bolin-dev/JavPack/main/libs/drive/JavPack.drive.lib.js
+// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
+// @connect         115.com
+// @connect         self
+// @run-at          document-body
+// @grant           GM_xmlhttpRequest
+// @grant           window.close
+// @license         GPL-3.0-only
+// @compatible      chrome
+// @compatible      edge
+// ==/UserScript==
+
+(function () {
+  const { searchParams } = new URL(location);
+  const pc = searchParams.get("pickcode");
+  if (!pc) return;
+
+  let loading = false;
+  const handleDel = async () => {
+    if (loading) return;
+    loading = true;
+
+    let res = await filesVideo(pc);
+    if (!res) return;
+
+    res = await files(res.parent_id);
+    res = res.path;
+    if (!res?.length) return;
+
+    rbDelete([res.at(-1).cid], res.at(-2).cid).then(window.close);
+  };
+
+  document
+    .querySelector(".vt-headline")
+    .insertAdjacentHTML("beforeend", `<a href="javascript:void(0);" class="btn-opendir" id="x-delete">删除目录</a>`);
+  document.querySelector("#x-delete").addEventListener("click", handleDel);
+})();
---
+++ drive/delete/README.md
@@ -0,0 +1,9 @@
+![JavPack](https://raw.githubusercontent.com/bolin-dev/JavPack/main/static/logo.png "logo")
+
+# Drive.delete
+
+播放页删除
+
+## 内容
+
+- 115 播放页删除视频文件夹
