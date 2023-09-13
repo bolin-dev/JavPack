777d85049622d92ea9e7034320592d2af6a4058e
bolinc <bolinchan2046@gmail.com>
2023-09-13 14:27:16 +0800

    refactor: javdb.match

--- javdb/match/JavDB.match.user.js
+++ javdb/match/JavDB.match.user.js
@@ -27,9 +27,7 @@
   const date = new Date().getDate();
   const cd = GM_getValue("CD");
 
-  if (!cd) {
-    GM_setValue("CD", date);
-  } else if (cd !== date) {
+  if (cd !== date) {
     for (const key of GM_listValues()) GM_deleteValue(key);
     GM_setValue("CD", date);
   }
@@ -39,6 +37,22 @@
 
   const childList = container.querySelectorAll(".item:not(.is-hidden)");
   if (!childList?.length) return;
+
+  GM_addStyle(
+    '.x-match:after{content:"";position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:3rem;height:3rem;background:url(/packs/media/images/btn-play-b414746c.svg) no-repeat center/contain;z-index:1}'
+  );
+
+  container.addEventListener("click", e => {
+    if (e.target.classList.contains("preview")) return;
+
+    const target = e.target.closest(".cover.x-match");
+    if (!target) return;
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    GM_openInTab(`https://v.anxia.com/?pickcode=${target.dataset.pc}`, { active: true, setParent: true });
+  });
 
   let loading = false;
   const listMatch = async () => {
@@ -88,6 +102,8 @@
   };
   parseItems(childList);
 
+  if (!document.querySelector("nav.pagination .pagination-next")) return;
+
   const callback = (mutationsList, observer) => {
     for (const { type, addedNodes } of mutationsList) {
       if (type !== "childList" || !addedNodes?.length) continue;
@@ -97,20 +113,4 @@
   };
   const mutationObserver = new MutationObserver(callback);
   mutationObserver.observe(container, { childList: true, attributes: false });
-
-  GM_addStyle(
-    '.x-match:after{content:"";position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:3rem;height:3rem;background:url(/packs/media/images/btn-play-b414746c.svg) no-repeat center/contain;z-index:1}'
-  );
-
-  container.addEventListener("click", e => {
-    if (e.target.classList.contains("preview")) return;
-
-    const target = e.target.closest(".cover.x-match");
-    if (!target) return;
-
-    e.preventDefault();
-    e.stopPropagation();
-
-    GM_openInTab(`https://v.anxia.com/?pickcode=${target.dataset.pc}`, { active: true, setParent: true });
-  });
 })();
