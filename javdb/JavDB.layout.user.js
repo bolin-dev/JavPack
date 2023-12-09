16c343edcc7922ccab6b7fdb0c5396c548e778a4
bolinc <bolinchan2046@gmail.com>
2023-12-10 06:07:48 +0800

    style: 元数据

--- javdb/JavDB.layout.user.js
+++ javdb/JavDB.layout.user.js
@@ -2,15 +2,15 @@
 // @name            JavDB.layout
 // @namespace       JavDB.layout@blc
 // @version         0.0.1
+// @author          blc
 // @description     布局调整
+// @match           https://javdb.com/*
 // @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
+// @resource        layout file:///Users/bolinc/Projects/JavPack/javdb/JavDB.layout.user.css
+// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
+// @run-at          document-start
 // @grant           GM_addStyle
 // @grant           GM_getResourceText
-// @author          blc
-// @resource        layout file:///Users/bolinc/Projects/JavPack/javdb/JavDB.layout.user.css
-// @match           https://javdb.com/*
-// @run-at          document-start
-// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
 // @license         GPL-3.0-only
 // @compatible      chrome last 2 versions
 // @compatible      edge last 2 versions
