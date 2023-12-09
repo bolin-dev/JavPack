834261d765cbae45f7b0831a913620d117b6dd34
bolinc <bolinchan2046@gmail.com>
2023-12-10 04:27:39 +0800

    style: nav

--- javdb/JavDB.nav.user.js
+++ javdb/JavDB.nav.user.js
@@ -2,19 +2,19 @@
 // @name            JavDB.nav
 // @namespace       JavDB.nav@blc
 // @version         0.0.1
+// @description     快捷翻页
+// @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
 // @author          blc
-// @description     快捷导航
-// @include         /^https:\/\/javdb\d*\.com\/(?!v\/)/
-// @icon            https://raw.githubusercontent.com/bolin-dev/JavPack/main/static/logo.png
-// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
+// @include         /^https:\/\/javdb\.com\/(?!v\/)/
 // @run-at          document-start
+// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
 // @license         GPL-3.0-only
-// @compatible      chrome
-// @compatible      edge
+// @compatible      chrome last 2 versions
+// @compatible      edge last 2 versions
 // ==/UserScript==
 
 (function () {
-  document.addEventListener("keyup", e => {
+  document.addEventListener("keyup", (e) => {
     const active = document.activeElement;
     if (["INPUT", "TEXTAREA"].includes(active.nodeName)) return;
 
@@ -27,6 +27,12 @@
     e.stopPropagation();
     e.stopImmediatePropagation();
 
-    document.querySelector(`nav.pagination .pagination-${e.key === "ArrowLeft" ? "previous" : "next"}`)?.click();
+    document
+      .querySelector(
+        `nav.pagination .pagination-${
+          e.key === "ArrowLeft" ? "previous" : "next"
+        }`,
+      )
+      ?.click();
   });
 })();
