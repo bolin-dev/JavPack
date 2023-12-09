33cfd675f971f48225beea8d7e30d7e98ea967d3
bolinc <bolinchan2046@gmail.com>
2023-12-10 02:57:07 +0800

    refactor: search

--- javdb/JavDB.search.user.js
+++ javdb/JavDB.search.user.js
@@ -2,32 +2,37 @@
 // @name            JavDB.search
 // @namespace       JavDB.search@blc
 // @version         0.0.1
-// @author          blc
-// @description     快捷搜索
-// @include         /^https:\/\/javdb\d*\.com/
-// @icon            https://raw.githubusercontent.com/bolin-dev/JavPack/main/static/logo.png
-// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
-// @run-at          document-start
+// @description     快捷键搜索
+// @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
 // @grant           GM_openInTab
+// @author          blc
+// @require         file:///Users/bolinc/Projects/JavPack/libs/JavPack.Util.lib.js
+// @match           https://javdb.com/*
+// @run-at          document-start
+// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
 // @license         GPL-3.0-only
-// @compatible      chrome
-// @compatible      edge
+// @compatible      chrome last 2 versions
+// @compatible      edge last 2 versions
 // ==/UserScript==
 
 (function () {
-  document.addEventListener("keydown", async e => {
+  document.addEventListener("keydown", async (e) => {
     if (e.ctrlKey && e.key === "/") {
       const text = (await navigator.clipboard.readText())?.trim();
       if (!text) return;
 
       const { origin, pathname } = location;
       const url = `${origin}/search?q=${text}`;
-      pathname === "/search" ? (location.href = url) : GM_openInTab(url, { active: true, setParent: true });
+      pathname === "/search" ? (location.href = url) : Util.openTab(url);
     }
   });
 
-  document.addEventListener("keyup", e => {
-    if (["INPUT", "TEXTAREA", "VIDEO", "AUDIO"].includes(document.activeElement.nodeName)) return;
-    if (e.key === "/") document.querySelector("#video-search")?.focus();
+  document.addEventListener("keyup", (e) => {
+    if (["INPUT", "TEXTAREA"].includes(document.activeElement.nodeName)) return;
+    if (e.key !== "/") return;
+
+    const input = document.querySelector("#video-search");
+    input.focus();
+    input.select();
   });
 })();
