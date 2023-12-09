365f090f1ee3b1d883715531e88614e4939beeaf
bolinc <bolinchan2046@gmail.com>
2023-12-10 06:14:08 +0800

    style: 元数据

--- javdb/JavDB.nav.user.js
+++ javdb/JavDB.nav.user.js
@@ -2,12 +2,12 @@
 // @name            JavDB.nav
 // @namespace       JavDB.nav@blc
 // @version         0.0.1
-// @description     快捷翻页
-// @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
 // @author          blc
+// @description     快捷翻页
 // @include         /^https:\/\/javdb\.com\/(?!v\/)/
-// @run-at          document-start
+// @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
 // @supportURL      https://t.me/+bAWrOoIqs3xmMjll
+// @run-at          document-start
 // @license         GPL-3.0-only
 // @compatible      chrome last 2 versions
 // @compatible      edge last 2 versions
