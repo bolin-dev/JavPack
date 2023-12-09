523594085b4ea9a959f2a30959472511b214c3d1
bolinc <bolinchan2046@gmail.com>
2023-12-10 06:11:07 +0800

    style: 元数据

--- javdb/JavDB.search.user.js
+++ javdb/JavDB.search.user.js
@@ -2,14 +2,14 @@
 // @name            JavDB.search
 // @namespace       JavDB.search@blc
 // @version         0.0.1
-// @description     快捷搜索
-// @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
-// @grant           GM_openInTab
 // @author          blc
-// @require         file:///Users/bolinc/Projects/JavPack/libs/JavPack.Util.lib.js
+// @description     快捷搜索
 // @match           https://javdb.com/*
-// @run-at          document-start
+// @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
+// @require         file:///Users/bolinc/Projects/JavPack/libs/JavPack.Util.lib.js
 // @supportURL      https://t.me/+bAWrOoIqs3xmMjll
+// @run-at          document-start
+// @grant           GM_openInTab
 // @license         GPL-3.0-only
 // @compatible      chrome last 2 versions
 // @compatible      edge last 2 versions
