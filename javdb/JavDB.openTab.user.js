eaa1ab950e8ead127a76542ce63b4ba86a4b80b9
bolinc <bolinchan2046@gmail.com>
2023-12-12 01:14:15 +0800

    Update @include and @exclude in JavDB.openTab.user.js

--- javdb/JavDB.openTab.user.js
+++ javdb/JavDB.openTab.user.js
@@ -4,7 +4,8 @@
 // @version         0.0.1
 // @author          blc
 // @description     新标签页打开
-// @include         /^https:\/\/javdb\.com\/(?!v\/)/
+// @match           https://javdb.com/*
+// @exclude         https://javdb.com/v/*
 // @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
 // @require         file:///Users/bolinc/Projects/JavPack/libs/JavPack.Util.lib.js
 // @supportURL      https://t.me/+bAWrOoIqs3xmMjll
@@ -16,7 +17,7 @@
 // ==/UserScript==
 
 (function () {
-  const selector = ":is(.movie-list, .actors, .section-container) a";
+  const selector = ":is(.actors, .movie-list, .section-container) a";
 
   const handleOpen = (e) => {
     const target = e.target.closest(selector);
