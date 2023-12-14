c708d2b3a62853449af8f3fa59e1985706efe389
bolinc <bolinchan2046@gmail.com>
2023-12-14 09:47:55 +0800

    Update JavDB filter script to run at document-end

--- javdb/JavDB.filter.user.js
+++ javdb/JavDB.filter.user.js
@@ -8,7 +8,7 @@
 // @exclude         https://javdb.com/v/*
 // @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
 // @supportURL      https://t.me/+bAWrOoIqs3xmMjll
-// @run-at          document-start
+// @run-at          document-end
 // @grant           GM_addStyle
 // @license         GPL-3.0-only
 // @compatible      chrome last 2 versions
@@ -17,33 +17,17 @@
 
 (function () {
   const hiddenList = [];
-  const highlightList = [];
+  const highlightList = [/\[[A-Z]+-\d+\]/];
   if (!hiddenList.length && !highlightList.length) return;
 
-  document.addEventListener("DOMContentLoaded", ({ target }) => {
-    const nodeList = target.querySelectorAll(".movie-list .item");
-    if (!nodeList.length) return;
+  const nodeList = document.querySelectorAll(".movie-list .item");
+  if (!nodeList.length) return;
 
-    GM_addStyle(
-      ".is-highlight{position:relative}.is-highlight::after{content:'';position:absolute;inset:0;z-index:-1;margin:-.375rem;border-radius:inherit;background:linear-gradient(45deg, #ff0000, #ff69b4, #ffa500)}",
-    );
-    filter(nodeList);
-    if (target.querySelector("nav.pagination .pagination-next")) observer();
-  });
+  GM_addStyle(
+    ".is-highlight{position:relative}.is-highlight::after{content:'';position:absolute;inset:0;z-index:-1;margin:-.375rem;border-radius:inherit;background:linear-gradient(45deg, #ff0000, #ff69b4, #ffa500)}",
+  );
 
-  function filter(nodeList) {
-    for (const node of nodeList) {
-      const res = parseNode(node);
-
-      if (hiddenList.some((item) => item.test(res))) {
-        node.classList.add("is-hidden");
-        continue;
-      }
-      if (highlightList.some((item) => item.test(res))) node.classList.add("is-highlight");
-    }
-  }
-
-  function parseNode(node) {
+  const parseNode = (node) => {
     const title = node.querySelector(".video-title");
     const code = title.querySelector("strong").textContent;
     const name = title.textContent.replace(code, "").trim();
@@ -54,20 +38,20 @@
     const date = node.querySelector(".meta").textContent.trim();
     const tags = [...node.querySelectorAll(".tags .tag")].map((tag) => tag.textContent);
     return `[${code}][${name}][${score}][${date}][${tags}]`;
-  }
+  };
 
-  function observer() {
-    const callback = (mutationList, observer) => {
-      for (const { type, addedNodes } of mutationList) {
-        if (type !== "childList" || !addedNodes.length) continue;
-        if (addedNodes.length < 40) observer.disconnect();
-        filter(addedNodes);
+  const filter = (nodeList) => {
+    for (const node of nodeList) {
+      const res = parseNode(node);
+
+      if (hiddenList.some((item) => item.test(res))) {
+        node.classList.add("is-hidden");
+        continue;
       }
-    };
-    const mutationObserver = new MutationObserver(callback);
+      if (highlightList.some((item) => item.test(res))) node.classList.add("is-highlight");
+    }
+  };
 
-    const target = document.querySelector(".movie-list");
-    const options = { childList: true, attributes: false, characterData: false };
-    mutationObserver.observe(target, options);
-  }
+  filter(nodeList);
+  window.addEventListener("loadmore", ({ detail }) => filter(detail));
 })();
