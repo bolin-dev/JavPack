3348c48c5854114cbec23c06b132e0b97f712b68
bolinc <bolinchan2046@gmail.com>
2023-12-14 16:24:25 +0800

    Refactor transToByte method and use arrow functions for getImg and openTab

--- libs/JavPack.Util.lib.js
+++ libs/JavPack.Util.lib.js
@@ -6,20 +6,6 @@ class Util {
   static varRep = /\${([^}]+)}/g;
   static zhReg = /中字|字幕|中文字幕|-c|(?<![a-z])ch/i;
 
-  static transToByteRules = [
-    { unit: /byte/i, trans: (size) => size },
-
-    { unit: /kb/i, trans: (size) => size * 1000 },
-    { unit: /mb/i, trans: (size) => size * 1000 ** 2 },
-    { unit: /gb/i, trans: (size) => size * 1000 ** 3 },
-    { unit: /tb/i, trans: (size) => size * 1000 ** 4 },
-
-    { unit: /kib/i, trans: (size) => size * 1024 },
-    { unit: /mib/i, trans: (size) => size * 1024 ** 2 },
-    { unit: /gib/i, trans: (size) => size * 1024 ** 3 },
-    { unit: /tib/i, trans: (size) => size * 1024 ** 4 },
-  ];
-
   static upLocal() {
     const date = new Date().getDate();
     if (localStorage.getItem("CD") === date.toString()) return;
@@ -45,15 +31,31 @@ class Util {
     };
   }
 
-  static transToByte(str) {
-    const num = str.match(this.numReg)?.[0] ?? 0;
-    if (num <= 0) return 0;
+  static useTransByte() {
+    const rules = [
+      { unit: /byte/i, trans: (size) => size },
 
-    const rule = this.transToByteRules.find(({ unit }) => unit.test(str));
-    return rule ? rule.trans(num).toFixed(2) : 0;
+      { unit: /kb/i, trans: (size) => size * 1000 },
+      { unit: /mb/i, trans: (size) => size * 1000 ** 2 },
+      { unit: /gb/i, trans: (size) => size * 1000 ** 3 },
+      { unit: /tb/i, trans: (size) => size * 1000 ** 4 },
+
+      { unit: /kib/i, trans: (size) => size * 1024 },
+      { unit: /mib/i, trans: (size) => size * 1024 ** 2 },
+      { unit: /gib/i, trans: (size) => size * 1024 ** 3 },
+      { unit: /tib/i, trans: (size) => size * 1024 ** 4 },
+    ];
+
+    return (str) => {
+      const num = str.match(this.numReg)?.[0] ?? 0;
+      if (num <= 0) return 0;
+
+      const rule = rules.find(({ unit }) => unit.test(str));
+      return rule ? rule.trans(num).toFixed(2) : 0;
+    };
   }
 
-  static getImg = (txt) => (txt.includes("//") ? txt : GM_getResourceURL(txt));
+  static getIcon = (txt) => (txt.includes("//") ? txt : GM_getResourceURL(txt));
 
   static notify(details) {
     if (typeof details === "string") details = { text: details };
@@ -65,7 +67,7 @@ class Util {
       highlight: false,
       title: GM_info.script.name,
       tag: GM_info.script.namespace,
-      image: this.getImg(details.icon),
+      image: this.getIcon(details.icon),
       ...details,
     });
   }
@@ -75,18 +77,14 @@ class Util {
     details = { icon: GM_info.script.icon, ...details };
 
     document.title = details.text;
-    const href = this.getImg(details.icon);
+    const href = this.getIcon(details.icon);
 
     const icons = document.querySelectorAll("link[rel*='icon']");
-    if (icons.length) {
-      return icons.forEach((item) => item.setAttribute("href", href));
-    }
+    if (icons.length) return icons.forEach((item) => item.setAttribute("href", href));
     GM_addElement(document.head, "link", { rel: "icon", href });
   }
 
-  static openTab = (url, active = true) => {
-    return GM_openInTab(url, { active, setParent: true });
-  };
+  static openTab = (url, active = true) => GM_openInTab(url, { active, setParent: true });
 
   static magnetSort = (a, b) => {
     if (a.zh !== b.zh) return a.zh ? -1 : 1;
