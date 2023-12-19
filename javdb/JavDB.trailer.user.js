9a0a5d85ae094467fc2a5c9a16cf83355efa7b5c
bolinc <bolinchan2046@gmail.com>
2023-12-19 18:19:12 +0800

    Add isElementInViewport function to check if an element is in the viewport

--- javdb/JavDB.trailer.user.js
+++ javdb/JavDB.trailer.user.js
@@ -135,6 +135,16 @@
   let lastY = null;
   let lastTime = null;
 
+  const isElementInViewport = (elem) => {
+    const rect = elem.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
   const handleMouseover = (e) => {
     if (currElem) return;
 
@@ -165,7 +175,7 @@
       speed = Math.sqrt(Math.pow(prevX - lastX, 2) + Math.pow(prevY - lastY, 2)) / (lastTime - prevTime);
     }
 
-    if (speed <= sensitivity) {
+    if (speed <= sensitivity && isElementInViewport(currElem)) {
       onHover(currElem);
     } else {
       prevX = lastX;
