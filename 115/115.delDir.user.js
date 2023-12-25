af804e124812332e6ad8f5a27e73f466295904e0
bolinc <bolinchan2046@gmail.com>
2023-12-26 07:24:56 +0800

    Update icon URL in 115.delDir.user.js

--- 115/115.delDir.user.js
+++ 115/115.delDir.user.js
@@ -5,7 +5,7 @@
 // @author          blc
 // @description     播放页删除目录
 // @match           https://v.anxia.com/*
-// @icon            https://s1.ax1x.com/2022/04/01/q5lzYn.png
+// @icon            https://v.anxia.com/m_r/favicon.ico
 // @require         https://raw.githubusercontent.com/bolin-dev/JavPack/main/libs/JavPack.Req.lib.js
 // @require         https://raw.githubusercontent.com/bolin-dev/JavPack/main/libs/JavPack.Req115.lib.js
 // @require         https://raw.githubusercontent.com/bolin-dev/JavPack/main/libs/JavPack.Util115.lib.js
