556a1430ee4231e5d9cd33fd85339ad2d9308535
bolinc <bolinchan2046@gmail.com>
2023-12-29 21:26:44 +0800

    Refactor UtilMagnet.btdig() method to include request headers

--- libs/JavPack.UtilMagnet.lib.js
+++ libs/JavPack.UtilMagnet.lib.js
@@ -1,20 +1,36 @@
 class UtilMagnet extends Req {
   static btdig(code) {
     const spaceReg = /\s/g;
-    return this.tasks(`https://btdig.com/search?q=${code}`, [
-      (dom) => {
-        return [...dom.querySelectorAll(".one_result")]
-          .map((item) => {
-            return {
-              name: item.querySelector(".torrent_name").textContent,
-              url: item.querySelector(".torrent_magnet a").href,
-              size: item.querySelector(".torrent_size").textContent.replace(spaceReg, ""),
-              files: item.querySelector(".torrent_files")?.textContent ?? "1",
-              date: item.querySelector(".torrent_age").textContent,
-            };
-          })
-          .filter(({ name }) => name.toUpperCase().includes(code.toUpperCase()));
+    const host = "https://btdig.com";
+
+    return this.tasks(
+      {
+        url: `${host}/search`,
+        params: { q: code },
+        headers: {
+          "Referer": `${host}/index.htm`,
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "same-origin",
+          "Sec-Fetch-User": "?1",
+          "Upgrade-Insecure-Requests": "1",
+        },
       },
-    ]);
+      [
+        (dom) => {
+          return [...dom.querySelectorAll(".one_result")]
+            .map((item) => {
+              return {
+                name: item.querySelector(".torrent_name").textContent,
+                url: item.querySelector(".torrent_magnet a").href,
+                size: item.querySelector(".torrent_size").textContent.replace(spaceReg, ""),
+                files: item.querySelector(".torrent_files")?.textContent ?? "1",
+                date: item.querySelector(".torrent_age").textContent,
+              };
+            })
+            .filter(({ name }) => name.toUpperCase().includes(code.toUpperCase()));
+        },
+      ],
+    );
   }
 }
