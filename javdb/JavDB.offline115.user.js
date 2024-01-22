f29993127c035db9d88f61d3d9a450eb8d5ba2be
bolinc <bolinchan2046@gmail.com>
2024-01-22 21:08:17 +0800

    Refactor getDetails function and use UtilDB.getDetails

--- javdb/JavDB.offline115.user.js
+++ javdb/JavDB.offline115.user.js
@@ -86,38 +86,6 @@
   const defaultVerifyOptions = Util115.defaultVerifyOptions;
   const defaultMagnetOptions = UtilDB.defaultMagnetOptions();
 
-  function getDetails(dom = document) {
-    const infoNode = dom.querySelector(".movie-panel-info");
-    const code = infoNode.querySelector(".first-block .value").textContent;
-
-    const titleNode = dom.querySelector(".title.is-4");
-    let title = titleNode.querySelector("strong").textContent;
-    title += (titleNode.querySelector(".origin-title") ?? titleNode.querySelector(".current-title")).textContent;
-    title = title.replaceAll(code, "").trim();
-
-    const details = {};
-    infoNode.querySelectorAll(".movie-panel-info > .panel-block").forEach((item) => {
-      const label = item.querySelector("strong")?.textContent;
-      const value = item.querySelector(".value")?.textContent;
-      if (!label || !value || value.includes("N/A")) return;
-
-      if (label === "日期:") details.date = value;
-      if (label === "導演:") details.director = value;
-      if (label === "片商:") details.maker = value;
-      if (label === "發行:") details.publisher = value;
-      if (label === "系列:") details.series = value;
-      if (label === "類別:") details.genres = value.split(",").map((item) => item.trim());
-      if (label !== "演員:") return;
-      details.actors = value
-        .split("\n")
-        .map((item) => item.trim())
-        .filter(Boolean);
-    });
-
-    const { regex, prefix } = UtilDB.codeParse(code);
-    return { infoNode, regex, prefix, code, title, create: new Date().toISOString().slice(0, 10), ...details };
-  }
-
   function getMagnets(dom = document) {
     return [...dom.querySelectorAll("#magnets-content > .item")]
       .map((item) => {
@@ -237,7 +205,7 @@
   GM_addElement(document.head, "link", { rel: "prefetch", href: GM_getResourceURL("error") });
   GM_addElement(document.head, "link", { rel: "prefetch", href: GM_getResourceURL("warn") });
 
-  const { infoNode, regex, ...details } = getDetails();
+  const { infoNode, regex, ...details } = UtilDB.getDetails();
   const { code } = details;
   const magnets = getMagnets();
   const actions = getActions(config, details, magnets);
