261d0f38cea081676656d2c4de912182f0e5a367
bolinc <bolinchan2046@gmail.com>
2024-02-29 07:13:20 +0800

    Update JavDB search user script

--- javdb/JavDB.search.user.js
+++ javdb/JavDB.search.user.js
@@ -6,9 +6,8 @@
 // @description     快捷搜索
 // @match           https://javdb.com/*
 // @icon            https://javdb.com/favicon.ico
-// @require         https://github.com/bolin-dev/JavPack/raw/main/libs/JavPack.Util.lib.js
 // @supportURL      https://t.me/+bAWrOoIqs3xmMjll
-// @run-at          document-body
+// @run-at          document-start
 // @grant           GM_openInTab
 // @license         GPL-3.0-only
 // @compatible      chrome last 2 versions
@@ -16,23 +15,25 @@
 // ==/UserScript==
 
 (function () {
-  const { origin, pathname } = location;
-  const exclude = ["INPUT", "TEXTAREA"];
-  const input = document.querySelector("#video-search");
-
   document.addEventListener("keydown", async (e) => {
     if (e.ctrlKey && e.code === "Slash") {
-      const txt = (await navigator.clipboard.readText())?.trim();
+      const txt = await navigator.clipboard.readText();
       if (!txt) return;
 
-      const url = `${origin}/search?q=${txt}`;
-      pathname === "/search" ? (location.href = url) : Util.openTab(url);
+      const url = `${location.origin}/search?q=${txt}`;
+      if (location.pathname !== "search") return GM_openInTab(url, { active: true, setParent: true });
+
+      location.href = url;
     }
   });
 
   document.addEventListener("keyup", (e) => {
     if (e.code !== "Slash") return;
-    if (exclude.includes(document.activeElement.nodeName)) return;
+    const { nodeName } = document.activeElement;
+    if (["INPUT", "TEXTAREA"].includes(nodeName)) return;
+
+    const input = document.querySelector("#video-search");
+    if (!input) return;
 
     input.focus();
     input.select();
