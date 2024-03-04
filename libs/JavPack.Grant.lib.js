c22b4b87ee0ef6188e33e9138e105148864ee9d4
bolinc <bolinchan2046@gmail.com>
2024-03-04 18:08:15 +0800

    Add notify method to Grant class

--- libs/JavPack.Grant.lib.js
+++ libs/JavPack.Grant.lib.js
@@ -1,3 +1,19 @@
 class Grant {
   static openTab = (url, active = true) => GM_openInTab(url, { active, setParent: true });
+
+  static notify = ({ text, icon, tag }) => {
+    const { name: defaultTitle, namespace: defaultTag, icon: defaultIcon } = GM_info.script;
+    icon = icon ? GM_getResourceURL(icon) : defaultIcon;
+    tag = tag ? `${defaultTag}:${tag}` : defaultTag;
+
+    GM_notification({
+      silent: true,
+      timeout: 3000,
+      highlight: false,
+      title: defaultTitle,
+      image: icon,
+      text,
+      tag,
+    });
+  };
 }
