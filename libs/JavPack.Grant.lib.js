7b4798786cb887ea68474532373e394bc17cb527
bolinc <bolinchan2046@gmail.com>
2024-03-05 10:15:08 +0800

    Refactor Grant.notify method in JavPack.Grant.lib.js

--- libs/JavPack.Grant.lib.js
+++ libs/JavPack.Grant.lib.js
@@ -1,18 +1,18 @@
 class Grant {
   static openTab = (url, active = true) => GM_openInTab(url, { active, setParent: true });
 
-  static notify = ({ text, icon, tag }) => {
-    const { name: defaultTitle, namespace: defaultTag, icon: defaultIcon } = GM_info.script;
-    icon = icon ? GM_getResourceURL(icon) : defaultIcon;
-    tag = tag ? `${defaultTag}:${tag}` : defaultTag;
+  static notify = ({ tag, icon: image, ...details }) => {
+    const { name: title, namespace, icon } = GM_info.script;
+    tag = tag ? `${namespace}:${tag}` : namespace;
+    image = image ? GM_getResourceURL(image) : icon;
 
     GM_notification({
+      title,
       silent: true,
       timeout: 3000,
       highlight: false,
-      title: defaultTitle,
-      image: icon,
-      text,
+      ...details,
+      image,
       tag,
     });
   };
