1485602ded09d7312e3767087506e1a5590b8a4b
bolinc <bolinchan2046@gmail.com>
2024-05-08 16:17:27 +0800

    Refactor md5_Utf8Encode function in JavPack.ReqDB.lib.js for improved readability

--- libs/JavPack.ReqDB.lib.js
+++ libs/JavPack.ReqDB.lib.js
@@ -195,21 +195,21 @@ function md5(string, bit = 32) {
   }
   function md5_Utf8Encode(string) {
     string = string.replace(/\r\n/g, "\n");
-    let utftext = "";
+    let utfText = "";
     for (let n = 0; n < string.length; n++) {
       let c = string.charCodeAt(n);
       if (c < 128) {
-        utftext += String.fromCharCode(c);
+        utfText += String.fromCharCode(c);
       } else if (c > 127 && c < 2048) {
-        utftext += String.fromCharCode((c >> 6) | 192);
-        utftext += String.fromCharCode((c & 63) | 128);
+        utfText += String.fromCharCode((c >> 6) | 192);
+        utfText += String.fromCharCode((c & 63) | 128);
       } else {
-        utftext += String.fromCharCode((c >> 12) | 224);
-        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-        utftext += String.fromCharCode((c & 63) | 128);
+        utfText += String.fromCharCode((c >> 12) | 224);
+        utfText += String.fromCharCode(((c >> 6) & 63) | 128);
+        utfText += String.fromCharCode((c & 63) | 128);
       }
     }
-    return utftext;
+    return utfText;
   }
   let x = [];
   let k;
