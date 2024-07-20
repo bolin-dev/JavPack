20686d0e5890efc2cc730928da1d077fc4fdb9f2
bolinc <bolinchan2046@gmail.com>
2024-07-20 13:52:26 +0800

    refactor: Update JavPack.ReqDB.lib.js to include current timestamp in sign calculation

--- libs/JavPack.ReqDB.lib.js
+++ libs/JavPack.ReqDB.lib.js
@@ -10,9 +10,9 @@ class ReqDB extends Req {
     if (CURR - ts <= 20) return localStorage.getItem(SIGN_KEY) || "";
 
     const secret = md5(
-      `${ts}71cf27bb3c0bcdf207b64abecddc970098c7421ee7203b9cdae54478478a199e7d5a6e1a57691123c1a931c057842fb73ba3b3c83bcd69c17ccf174081e3d8aa`,
+      `${CURR}71cf27bb3c0bcdf207b64abecddc970098c7421ee7203b9cdae54478478a199e7d5a6e1a57691123c1a931c057842fb73ba3b3c83bcd69c17ccf174081e3d8aa`,
     );
-    const sign = `${ts}.lpw6vgqzsp.${secret}`;
+    const sign = `${CURR}.lpw6vgqzsp.${secret}`;
 
     localStorage.setItem(TS_KEY, CURR);
     localStorage.setItem(SIGN_KEY, sign);
