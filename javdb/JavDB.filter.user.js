0cc8ee4eeae6226fdf291f4ee84a6430bac7ae33
bolinc <bolinchan2046@gmail.com>
2024-07-21 14:21:44 +0800

    refactor: Update JavDB.filter.user.js to optimize code for better performance

--- javdb/JavDB.filter.user.js
+++ javdb/JavDB.filter.user.js
@@ -7,39 +7,17 @@
 // @match           https://javdb.com/*
 // @exclude         https://javdb.com/v/*
 // @icon            https://javdb.com/favicon.ico
-// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
 // @run-at          document-end
-// @grant           GM_addStyle
-// @license         GPL-3.0-only
-// @compatible      chrome last 2 versions
-// @compatible      edge last 2 versions
 // ==/UserScript==
 
 (function () {
-  const hiddenList = [];
-  const highlightList = [];
-  if (!hiddenList.length && !highlightList.length) return;
+  const config = [];
+  if (!config.length) return;
 
   const nodeList = document.querySelectorAll(".movie-list .item");
   if (!nodeList.length) return;
 
-  GM_addStyle(`
-  .is-highlight {
-    position: relative;
-
-    &::after {
-      position: absolute;
-      inset: 0;
-      z-index: -1;
-      margin: -0.375rem;
-      content: "";
-      background: linear-gradient(45deg, #f00, #ff69b4, #ffa500);
-      border-radius: inherit;
-    }
-  }
-  `);
-
-  const parseNode = (node) => {
+  const parse = (node) => {
     const titleNode = node.querySelector(".video-title");
     const code = titleNode.querySelector("strong").textContent;
     const title = titleNode.textContent.split(code).pop().trim();
@@ -52,18 +30,13 @@
     return `[${code}][${title}][${score}][${date}][${tags}]`;
   };
 
-  const filter = (nodeList) => {
-    for (const node of nodeList) {
-      const parseStr = parseNode(node);
-
-      if (hiddenList.some((item) => item.test(parseStr))) {
-        node.classList.add("is-hidden");
-        continue;
-      }
-      if (highlightList.some((item) => item.test(parseStr))) node.classList.add("is-highlight");
-    }
+  const filter = (list) => {
+    list.forEach((item) => {
+      const txt = parse(item);
+      if (config.some((cfg) => cfg.test(txt))) item.classList.add("is-hidden");
+    });
   };
 
   filter(nodeList);
-  window.addEventListener("scroll.loadmore", ({ detail }) => filter(detail));
+  window.addEventListener("JavDB.scroll", ({ detail }) => filter(detail));
 })();
