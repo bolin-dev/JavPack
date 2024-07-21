04cfce01258fafec89507085b9ef607f272c6a95
bolinc <bolinchan2046@gmail.com>
2024-07-21 14:23:50 +0800

    refactor: Update JavDB.layout.user.js and JavDB.style.user.js to use external CSS files for better maintainability

--- javdb/JavDB.layout.user.js
+++ javdb/JavDB.layout.user.js
@@ -7,13 +7,9 @@
 // @match           https://javdb.com/*
 // @icon            https://javdb.com/favicon.ico
 // @resource        layout https://github.com/bolin-dev/JavPack/raw/main/static/JavDB.layout.user.css
-// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
 // @run-at          document-start
 // @grant           GM_getResourceText
 // @grant           GM_addStyle
-// @license         GPL-3.0-only
-// @compatible      chrome last 2 versions
-// @compatible      edge last 2 versions
 // ==/UserScript==
 
 GM_addStyle(GM_getResourceText("layout"));
--- javdb/JavDB.style.user.js
+++ javdb/JavDB.style.user.js
@@ -7,13 +7,9 @@
 // @match           https://javdb.com/*
 // @icon            https://javdb.com/favicon.ico
 // @resource        style https://github.com/bolin-dev/JavPack/raw/main/static/JavDB.style.user.css
-// @supportURL      https://t.me/+bAWrOoIqs3xmMjll
 // @run-at          document-start
 // @grant           GM_getResourceText
 // @grant           GM_addStyle
-// @license         GPL-3.0-only
-// @compatible      chrome last 2 versions
-// @compatible      edge last 2 versions
 // ==/UserScript==
 
 GM_addStyle(GM_getResourceText("style"));
