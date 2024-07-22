0811070794167f26f65110688404a0167545a5aa
bolinc <bolinchan2046@gmail.com>
2024-07-22 22:31:18 +0800

    refactor: Update JavPack.Grant.lib.js to improve code structure and notification handling

--- libs/JavPack.Grant.lib.js
+++ libs/JavPack.Grant.lib.js
@@ -1,18 +1,23 @@
 class Grant {
   static openTab = (url, active = true) => GM_openInTab(url, { active, setParent: true });
 
-  static notify = ({ icon: image, ...details }) => {
-    const { name: title, namespace: tag, icon } = GM_info.script;
-    image = image ? GM_getResourceURL(image) : icon;
+  static notify = (options) => {
+    if (typeof options === "string") options = { text: options };
+    options.text = options.text ?? options.msg;
+    if (!options.text) return;
+
+    if (!options.image) {
+      const icon = options.icon ?? options.status;
+      options.image = icon ? GM_getResourceURL(icon) : GM_info.script.icon;
+    }
 
     GM_notification({
-      tag,
-      image,
-      title,
+      tag: GM_info.script.namespace,
+      title: GM_info.script.name,
       silent: true,
       timeout: 3000,
       highlight: false,
-      ...details,
+      ...options,
     });
   };
 }
