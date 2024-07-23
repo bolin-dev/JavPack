cf22a5fbafdc55552a40c26d0298362a845f0f19
bolinc <bolinchan2046@gmail.com>
2024-07-23 14:23:23 +0800

    refactor: Add lazy loading for images in JavDB.offline115.user.js

--- javdb/JavDB.match115.user.js
+++ javdb/JavDB.match115.user.js
@@ -146,3 +146,156 @@ const listenClick = (onclose) => {
   labelNode.addEventListener("click", matchCode);
   window.addEventListener("beforeunload", () => CHANNEL.postMessage(MID));
 })();
+
+(function () {
+  if (IS_DETAIL) return;
+
+  const MOVIE_SELECTOR = ".movie-list .item";
+  const movieNodeList = document.querySelectorAll(MOVIE_SELECTOR);
+  if (!movieNodeList.length) return;
+
+  const insertMatch = (nodeList) => {
+    const matchTxt = `<a href="${VOID}" class="${TARGET_CLASS} tag">匹配中</a>&nbsp;`;
+    const insert = (node) => node.querySelector(".video-title").insertAdjacentHTML("afterbegin", matchTxt);
+    nodeList.forEach(insert);
+  };
+
+  const useQueryMatch = () => {
+    let lock = false;
+    let queue = [];
+
+    const getList = (nodeList) => {
+      return [...nodeList].map((node) => {
+        const mid = node.querySelector("a").href.split("/").pop();
+        const titleNode = node.querySelector(".video-title");
+        const tagNode = titleNode.querySelector(`.${TARGET_CLASS}`);
+        const code = titleNode.querySelector("strong").textContent;
+        const { prefix, regex } = Util.codeParse(code);
+        return { code, prefix, node, mid, regex, tagNode };
+      });
+    };
+
+    const setMatch = ({ node, mid, regex, tagNode }, res) => {
+      node.classList.add(`x-${mid}`);
+      res = res.filter((item) => regex.test(item.n));
+
+      let pc = "";
+      let cid = "";
+      let title = "";
+      let textContent = "未匹配";
+      let className = "is-normal";
+
+      if (res.length) {
+        const zhItem = res.find(({ n }) => Magnet.zhReg.test(n));
+        const crackItem = res.find(({ n }) => Magnet.crackReg.test(n));
+        const bothItem = res.find(({ n }) => Magnet.zhReg.test(n) && Magnet.crackReg.test(n));
+        const currItem = bothItem ?? zhItem ?? crackItem ?? res[0];
+
+        pc = currItem.pc;
+        cid = currItem.cid;
+        textContent = "已匹配";
+        title = `[${currItem.t}] ${currItem.n}`;
+        className = bothItem ? "is-danger" : zhItem ? "is-warning" : crackItem ? "is-info" : "is-success";
+      }
+
+      tagNode.title = title;
+      tagNode.dataset.pc = pc;
+      tagNode.dataset.cid = cid;
+      tagNode.textContent = textContent;
+      tagNode.className = `${TARGET_CLASS} tag ${className}`;
+    };
+
+    const localMatch = (list) => {
+      return list.filter(({ code, prefix, ...item }) => {
+        const res = GM_getValue(code) ?? GM_getValue(prefix);
+        return res ? setMatch(item, res) : true;
+      });
+    };
+
+    const queryMatch = ([prefix, list]) => {
+      return Req115.videosSearch(prefix).then(({ data }) => {
+        data = data.map(({ pc, cid, t, n }) => ({ pc, cid, t, n }));
+        GM_setValue(prefix, data);
+        list.forEach((item) => setMatch(item, data));
+      });
+    };
+
+    const takeQueue = async () => {
+      const tasks = localMatch(queue.splice(0, 3));
+      const prefixMap = tasks.reduce((acc, { prefix, ...item }) => {
+        acc[prefix] ??= [];
+        acc[prefix].push(item);
+        return acc;
+      }, {});
+
+      await Promise.allSettled(Object.entries(prefixMap).map(queryMatch));
+      if (queue.length) return takeQueue();
+    };
+
+    return async (nodeList) => {
+      if (!nodeList?.length) return;
+
+      if (typeof nodeList === "string") nodeList = document.querySelectorAll(`.movie-list .x-${nodeList}`);
+      if (!nodeList.length) return;
+
+      const list = getList(nodeList);
+      const tasks = localMatch(list);
+      if (!tasks.length) return;
+
+      queue.push(...tasks);
+      if (lock) return;
+
+      lock = true;
+      await takeQueue();
+      lock = false;
+    };
+  };
+
+  const addQueue = useQueryMatch();
+
+  const observer = (onIntersected) => {
+    const callback = (entries, observer) => {
+      const intersected = [];
+
+      entries.forEach(({ isIntersecting, target }) => {
+        if (!isIntersecting) return;
+        observer.unobserve(target);
+        intersected.push(target);
+      });
+
+      onIntersected(intersected);
+    };
+
+    const observer = new IntersectionObserver(callback, { threshold: 0.2 });
+    const observe = (nodeList) => nodeList.forEach((node) => observer.observe(node));
+
+    observe(movieNodeList);
+    window.addEventListener("JavDB.scroll", ({ detail }) => observe(detail));
+  };
+
+  observer((nodeList) => {
+    insertMatch(nodeList);
+    addQueue(nodeList);
+  });
+
+  CHANNEL.onmessage = ({ data }) => addQueue(data);
+
+  const matchPrefix = (target) => {
+    const node = target.closest(MOVIE_SELECTOR);
+    const mid = node.querySelector("a").href.split("/").pop();
+    const code = node.querySelector(".video-title strong").textContent;
+    const { prefix } = Util.codeParse(code);
+    GM_deleteValue(code);
+
+    Req115.videosSearch(prefix).then(({ data }) => {
+      data = data.map(({ pc, cid, t, n }) => ({ pc, cid, t, n }));
+      GM_setValue(prefix, data);
+
+      addQueue(mid);
+      CHANNEL.postMessage(mid);
+    });
+  };
+
+  listenClick(matchPrefix);
+  unsafeWindow["reMatch"] = matchPrefix;
+})();
