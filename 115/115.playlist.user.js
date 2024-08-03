3206e2c81d5174ab4fb471701f73e5a1d6ff14fa
bolinc <bolinchan2046@gmail.com>
2024-08-03 14:05:30 +0800

    refactor: Optimize playlist user script for better performance and readability

--- 115/115.playlist.user.js
+++ 115/115.playlist.user.js
@@ -8,85 +8,82 @@
 // @match           https://v.anxia.com/*
 // @icon            https://115.com/favicon.ico
 // @require         https://github.com/bolin-dev/JavPack/raw/main/libs/JavPack.Grant.lib.js
-// @require         https://github.com/bolin-dev/JavPack/raw/main/libs/JavPack.Req.lib.js
-// @require         https://github.com/bolin-dev/JavPack/raw/main/libs/JavPack.Req115.lib.js
-// @connect         115.com
 // @run-at          document-end
-// @grant           GM_xmlhttpRequest
+// @grant           GM_deleteValue
 // @grant           GM_openInTab
+// @grant           GM_getValue
+// @grant           GM_setValue
 // ==/UserScript==
 
 (function () {
-  const filterBox = document.querySelector("#js_filter_box");
-  if (!filterBox) return;
+  const filterNode = document.querySelector("#js_filter_box");
+  if (!filterNode) return;
 
-  document.addEventListener(
-    "click",
-    (e) => {
-      if (!filterBox.querySelector(".selected[val='4']")) return;
+  const parseList = (list) => {
+    const res = [];
 
-      const target = e.target.closest(".list-contents .file-name .name");
-      if (!target) return;
+    list.forEach((item) => {
+      const pc = item.getAttribute("pick_code");
+      if (pc) res.push({ n: item.title, pc });
+    });
 
-      const pickcode = target.closest("li").getAttribute("pick_code");
-      if (!pickcode) return;
+    return res;
+  };
 
-      e.preventDefault();
-      e.stopPropagation();
+  const onclick = (e) => {
+    if (!filterNode.querySelector(".selected[val='4']")) return;
 
-      const { searchParams } = new URL(window.parent.location);
-      const cid = searchParams.get("cid");
-      Grant.openTab(`https://v.anxia.com/?pickcode=${pickcode}&cid=${cid}`);
-    },
-    true,
-  );
+    const target = e.target.closest(".list-contents .file-name .name");
+    if (!target) return;
+
+    const pickcode = target.closest("li").getAttribute("pick_code");
+    if (!pickcode) return;
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    const { searchParams } = new URL(window.parent.location);
+    const cid = searchParams.get("cid");
+
+    const nodeList = target.closest(".list-contents ul").querySelectorAll("li");
+    GM_setValue(cid, parseList(nodeList));
+
+    const tab = Grant.openTab(`https://v.anxia.com/?pickcode=${pickcode}&cid=${cid}`);
+    tab.onclose = () => GM_deleteValue(cid);
+  };
+
+  document.addEventListener("click", onclick, true);
 })();
 
 (function () {
-  const playlist = document.querySelector("#js-video_list");
-  if (!playlist) return;
+  const playlistNode = document.querySelector("#js-video_list");
+  if (!playlistNode) return;
 
-  const fullscreen = document.querySelector(".bar-side .btn-opt[rel='fullscreen']");
+  const fullscreenNode = document.querySelector(".bar-side .btn-opt[rel='fullscreen']");
 
-  document.addEventListener("keyup", ({ code }) => {
-    if (code === "KeyF") return fullscreen?.click();
+  const onkeyup = ({ code }) => {
+    if (code === "KeyF") return fullscreenNode?.click();
 
-    const curr = playlist.querySelector("li.hover");
-    if (code === "BracketRight") return curr.nextElementSibling?.querySelector("a").click();
-    if (code === "BracketLeft") return curr.previousElementSibling?.querySelector("a").click();
-  });
+    const currNode = playlistNode.querySelector("li.hover");
+    if (code === "BracketRight") return currNode?.nextElementSibling?.querySelector("a").click();
+    if (code === "BracketLeft") return currNode?.previousElementSibling?.querySelector("a").click();
+  };
+
+  document.addEventListener("keyup", onkeyup);
 
   const { searchParams } = new URL(location);
-  const pickcode = searchParams.get("pickcode");
   const cid = searchParams.get("cid");
-  if (!cid || !pickcode) return;
+  if (!cid) return;
 
-  let videos = [];
-  let loaded = false;
+  const pickcode = searchParams.get("pickcode");
+  const playlist = GM_getValue(cid);
+  if (!pickcode || !playlist?.length || !playlist.some((it) => it.pc === pickcode)) return;
 
-  Req115.videos(cid).then(({ data }) => {
-    videos = data;
-    handleReplaceVideos();
-  });
-
-  const obs = new MutationObserver((mutations, observer) => {
-    mutations.forEach(({ type }) => {
-      if (type !== "childList") return;
-      loaded = true;
-      handleReplaceVideos();
-      observer.disconnect();
-    });
-  });
-  obs.observe(playlist, { childList: true, attributes: false, characterData: false });
-
-  function handleReplaceVideos() {
-    if (!loaded || !videos.length) return;
-
-    playlist.innerHTML = `
-    ${videos
+  const repList = (list, curr) => {
+    playlistNode.innerHTML = list
       .map(({ pc, n }) => {
         return `
-        <li pickcode="${pc}" style="padding:0px" class="${pc === pickcode ? "hover" : ""}">
+        <li pickcode="${pc}" style="padding:0px" class="${pc === curr ? "hover" : ""}">
           <a
             href="/?pickcode=${pc}&cid=${cid}"
             style="height:auto;text-decoration:none;padding:5px 0 5px 5px;"
@@ -97,7 +94,19 @@
         </li>
         `;
       })
-      .join("")}
-    `;
-  }
+      .join("");
+  };
+
+  const obList = () => {
+    const obs = new MutationObserver((mutations, observer) => {
+      mutations.forEach(({ type }) => {
+        if (type !== "childList") return;
+        observer.disconnect();
+        repList(playlist, pickcode);
+      });
+    });
+    obs.observe(playlistNode, { childList: true, attributes: false, characterData: false });
+  };
+
+  playlistNode.querySelector("li") ? repList(playlist, pickcode) : obList();
 })();
