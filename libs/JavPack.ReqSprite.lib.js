c158b6cb342cc556bb1aa907b8a636b47cae266e
bolinc <bolinchan2046@gmail.com>
2024-12-18 12:30:16 +0800

    refactor: 重构 javbee 方法，简化代码并优化图像获取逻辑

--- libs/JavPack.ReqSprite.lib.js
+++ libs/JavPack.ReqSprite.lib.js
@@ -1,65 +1,17 @@
 class ReqSprite extends Req {
-  static blogjav(code) {
-    const word = code.replace("HEYZO-", "HEYZO ");
+  static async javbee(code) {
+    const res = await this.request(`https://javbee.me/search?keyword=${code}`);
+    const url = res.querySelector(".content-main .card .images-description a")?.href;
+    if (!url) throw new Error("Not found target");
 
-    return this.tasks({ url: "https://blogjav.net/", params: { s: word } }, [
-      (dom) => {
-        const list = dom.querySelectorAll("#main .entry-title > a");
-        const item = [...list].find((item) => {
-          const title = item.textContent.toUpperCase();
-          return title.includes(word) || title.includes(code);
-        });
+    const target = await this.request(url);
+    const sprite = target.querySelector(".fileviewer-file img")?.src;
+    if (!sprite) throw new Error("Not found sprite");
 
-        return item?.href;
-      },
-      (dom) => {
-        let img = dom.querySelector("#main .entry-content p > a img");
-        if (!img) return;
-
-        img = img.dataset.src ?? img.dataset.lazySrc;
-        if (!img) return;
-
-        img = img.replace("//t", "//img").replace("/thumbs/", "/images/");
-        return { method: "HEAD", url: img };
-      },
-      (url) => url,
-    ]);
+    return sprite;
   }
 
-  static javstore(code) {
-    const word = code.replace("HEYZO-", "HEYZO ");
-
-    return this.tasks(`https://javstore.net/search/${word}.html`, [
-      (dom) => {
-        const list = dom.querySelectorAll("#content_news li > a");
-        const item = [...list].find((item) => {
-          const title = item.title.toUpperCase();
-          return title.includes(word) || title.includes(code);
-        });
-
-        return item?.href;
-      },
-      (dom) => {
-        const link = dom.querySelector(".news > a");
-        const regex = /\.(jpg|png)$/i;
-
-        let img = link.href;
-        if (!img) return;
-
-        if (regex.test(img)) {
-          img = img.replace("//pixhost.to/show/", "//img89.pixhost.to/images/");
-          return { method: "HEAD", url: img };
-        }
-
-        img = link.querySelector("img")?.src;
-        if (!img) return;
-
-        if (regex.test(img)) {
-          img = img.replace(".th.", ".");
-          return { method: "HEAD", url: img };
-        }
-      },
-      (url) => url,
-    ]);
+  static getSprite(code) {
+    return this.javbee(code);
   }
 }
