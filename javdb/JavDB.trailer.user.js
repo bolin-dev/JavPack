68782aed51584bcb1a47142ce703f66644da9afb
bolinc <bolinchan2046@gmail.com>
2024-12-18 09:08:45 +0800

    refactor: 重构视频控制逻辑，优化音量和播放时间的处理

--- javdb/JavDB.trailer.user.js
+++ javdb/JavDB.trailer.user.js
@@ -9,14 +9,14 @@
 // @require         https://github.com/bolin-dev/JavPack/raw/main/libs/JavPack.Req.lib.js
 // @require         https://github.com/bolin-dev/JavPack/raw/main/libs/JavPack.ReqTrailer.lib.js
 // @require         https://github.com/bolin-dev/JavPack/raw/main/libs/JavPack.Util.lib.js
-// @connect         pacopacomama.com
 // @connect         caribbeancom.com
+// @connect         pacopacomama.com
 // @connect         tokyo-hot.com
 // @connect         10musume.com
 // @connect         muramura.tv
-// @connect         1pondo.tv
-// @connect         heyzo.com
 // @connect         dmm.co.jp
+// @connect         heyzo.com
+// @connect         1pondo.tv
 // @connect         self
 // @run-at          document-end
 // @grant           GM_xmlhttpRequest
@@ -44,7 +44,6 @@ const getDetails = (dom = document) => {
   const origin = titleNode.querySelector(".origin-title");
   const current = titleNode.querySelector(".current-title");
   const title = (origin ?? current).textContent.replaceAll(code, "").trim();
-
   const cover = dom.querySelector(".video-cover")?.src ?? "";
 
   const studio =
@@ -61,16 +60,47 @@ const getDetails = (dom = document) => {
 };
 
 const useVideo = () => {
-  const onKeydown = (e) => {
+  const toggleMute = (e) => {
     e.preventDefault();
     e.stopPropagation();
 
-    const { code, target } = e;
-    if (code === "KeyM") target.muted = !target.muted;
-    if (["KeyW", "ArrowUp"].includes(code)) target.volume += 0.1;
-    if (["KeyS", "ArrowDown"].includes(code)) target.volume -= 0.1;
-    if (["KeyA", "ArrowLeft"].includes(code)) target.currentTime -= 2;
-    if (["KeyD", "ArrowRight"].includes(code)) target.currentTime += 4;
+    const { target } = e;
+    target.muted = !target.muted;
+  };
+
+  const togglePlay = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const { target } = e;
+    target.paused ? target.play() : target.pause();
+  };
+
+  const changeVolume = (e, step) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const { target } = e;
+    target.volume = target.volume + step;
+  };
+
+  const changeTime = (e, step) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const { target } = e;
+    target.currentTime = target.currentTime + step;
+  };
+
+  const onKeydown = (e) => {
+    const code = e.code;
+
+    if (["KeyM"].includes(code)) return toggleMute(e);
+    if (["Space"].includes(code)) return togglePlay(e);
+    if (["KeyW", "ArrowUp"].includes(code)) return changeVolume(e, 0.1);
+    if (["KeyS", "ArrowDown"].includes(code)) return changeVolume(e, -0.1);
+    if (["KeyA", "ArrowLeft"].includes(code)) return changeTime(e, -2);
+    if (["KeyD", "ArrowRight"].includes(code)) return changeTime(e, 4);
   };
 
   const onVolumechange = ({ target }) => localStorage.setItem("volume", target.volume);
@@ -99,9 +129,20 @@ const useVideo = () => {
   const container = document.querySelector(".column-video-cover");
   if (!container) return;
 
+  const mid = unsafeWindow.appData.split("/").at(-1);
+  if (!mid) return;
+
   const setTrailer = ({ sources, cover }) => {
-    const createVideo = useVideo();
-    const video = createVideo(sources, cover);
+    const video = useVideo()(sources, cover);
+
+    video.addEventListener("play", ({ target }) => {
+      target.style.zIndex = 11;
+    });
+
+    video.addEventListener("pause", ({ target }) => {
+      target.style.zIndex = "auto";
+    });
+
     container.querySelector("a").insertAdjacentElement("beforeend", video);
 
     container.addEventListener("click", (e) => {
@@ -109,19 +150,14 @@ const useVideo = () => {
 
       e.preventDefault();
       e.stopPropagation();
+
       video.focus();
-
-      if (video.paused) {
-        video.style.zIndex = 11;
-        return video.play();
-      }
-
-      video.style.zIndex = "auto";
-      video.pause();
+      video.paused ? video.play() : video.pause();
     });
+
+    video.focus();
   };
 
-  const mid = unsafeWindow.appData.split("/").filter(Boolean).pop();
   let details = GM_getValue(mid);
 
   if (!details) {
@@ -131,15 +167,13 @@ const useVideo = () => {
 
   if (details.sources.length) return setTrailer(details);
 
-  const onsuccess = (sources) => {
-    details.sources = sources;
-    GM_setValue(mid, details);
-    setTrailer(details);
-  };
-
   ReqTrailer.getTrailer(details)
-    .then(onsuccess)
-    .catch((err) => console.error(err.message));
+    .then((sources) => {
+      details.sources = sources;
+      GM_setValue(mid, details);
+      setTrailer(details);
+    })
+    .catch((err) => console.warn(err.message));
 })();
 
 (function () {
@@ -161,8 +195,9 @@ const useVideo = () => {
     const interval = 200;
     const sensitivity = 0.02;
 
-    let scrollTimer = null;
     let isScrolling = false;
+    let scrollTimer = null;
+    const scrollEndDelay = 500;
 
     const onMousemove = (e) => {
       lastX = e.pageX;
@@ -182,7 +217,10 @@ const useVideo = () => {
 
     const clearTrack = (elem) => {
       elem.removeEventListener("mousemove", onMousemove);
+      if (!trackSpeedInterval) return;
+
       clearInterval(trackSpeedInterval);
+      trackSpeedInterval = null;
     };
 
     const trackSpeed = () => {
@@ -234,12 +272,10 @@ const useVideo = () => {
     const onScroll = () => {
       isScrolling = true;
       clearTimeout(scrollTimer);
-    };
 
-    const onScrollEnd = () => {
       scrollTimer = setTimeout(() => {
         isScrolling = false;
-      }, 500);
+      }, scrollEndDelay);
     };
 
     const onOut = () => {
@@ -254,30 +290,27 @@ const useVideo = () => {
     document.addEventListener("mouseout", onMouseout);
     document.addEventListener("visibilitychange", onOut);
     window.addEventListener("scroll", onScroll);
-    window.addEventListener("scrollend", onScrollEnd);
     window.addEventListener("blur", onOut);
   };
 
   const TAG = "x-hovered";
-
   const createVideo = useVideo();
+  let audioContext = null;
 
   const setTrailer = (elem, { sources, cover }) => {
     if (!elem.classList.contains(TAG)) return;
 
     const video = createVideo(sources.toReversed(), cover);
-    elem.append(video);
 
     video.muted = true;
+    video.autoplay = true;
     video.currentTime = 4;
+
+    elem.append(video);
     video.focus();
-    video.play();
 
-    const ctx = new AudioContext();
-    const canAutoPlay = ctx.state === "running";
-    ctx.close();
-
-    if (canAutoPlay) video.muted = false;
+    if (!audioContext) audioContext = new AudioContext();
+    if (audioContext.state === "running") video.muted = false;
   };
 
   const onEnter = async (elem) => {
@@ -289,11 +322,10 @@ const useVideo = () => {
 
     if (!details) {
       try {
-        const dom = await Req.request(url);
-        details = getDetails(dom);
+        details = await Req.tasks(url, [getDetails]);
         GM_setValue(mid, details);
       } catch (err) {
-        return console.error(err.message);
+        return console.warn(err.message);
       }
     }
 
@@ -305,7 +337,7 @@ const useVideo = () => {
         GM_setValue(mid, details);
         setTrailer(elem, details);
       })
-      .catch((err) => console.error(err.message));
+      .catch((err) => console.warn(err.message));
   };
 
   const onLeave = (elem) => {
