6091517ed08647762a5ad321123719cc270aa021
bolinc <bolinchan2046@gmail.com>
2024-12-18 18:02:30 +0800

    refactor: 重构 btdig 和 nyaa 方法，简化代码并优化数据提取逻辑

--- libs/JavPack.ReqMagnet.lib.js
+++ libs/JavPack.ReqMagnet.lib.js
@@ -1,36 +1,37 @@
 class ReqMagnet extends Req {
   static btdig(code) {
-    const REG = /\s/g;
-    const HOST = "https://btdig.com";
-
-    return this.tasks(
-      {
-        url: `${HOST}/search`,
-        params: { q: code },
-        headers: {
-          "Referer": `${HOST}/index.htm`,
-          "Sec-Fetch-Dest": "document",
-          "Sec-Fetch-Mode": "navigate",
-          "Sec-Fetch-Site": "same-origin",
-          "Sec-Fetch-User": "?1",
-          "Upgrade-Insecure-Requests": "1",
-        },
+    return this.tasks(`https://btdig.com/search?order=0&q=${code}`, [
+      (dom) => {
+        return [...dom.querySelectorAll(".one_result")]
+          .map((node) => {
+            return {
+              url: node.querySelector(".torrent_magnet a")?.href,
+              name: node.querySelector(".torrent_name")?.textContent.trim() ?? "",
+              files: node.querySelector(".torrent_files")?.textContent.trim() ?? "",
+              size: node.querySelector(".torrent_size")?.textContent.replace(/\s/g, "") ?? "",
+              date: node.querySelector(".torrent_age")?.textContent.trim() ?? "",
+            };
+          })
+          .filter(({ url, name }) => url && name.toUpperCase().includes(code));
       },
-      [
-        (dom) => {
-          return [...dom.querySelectorAll(".one_result")]
-            .map((item) => {
-              return {
-                name: item.querySelector(".torrent_name").textContent,
-                url: item.querySelector(".torrent_magnet a").href,
-                size: item.querySelector(".torrent_size").textContent.replace(REG, ""),
-                files: item.querySelector(".torrent_files")?.textContent ?? "1",
-                date: item.querySelector(".torrent_age").textContent,
-              };
-            })
-            .filter(({ name }) => name.toUpperCase().includes(code));
-        },
-      ],
-    );
+    ]);
+  }
+
+  static nyaa(code) {
+    return this.tasks(`https://sukebei.nyaa.si/?f=0&c=2_2&q=${code}`, [
+      (dom) => {
+        return [...dom.querySelectorAll(".torrent-list tbody > tr")]
+          .map((node) => {
+            const tds = node.querySelectorAll("td");
+            return {
+              url: tds[2].querySelectorAll("a")?.[1]?.href,
+              name: tds[1].textContent?.trim() ?? "",
+              size: tds[3].textContent?.replace(/\s/g, "") ?? "",
+              date: tds[4].textContent?.trim() ?? "",
+            };
+          })
+          .filter(({ url, name }) => url && name.toUpperCase().includes(code));
+      },
+    ]);
   }
 }
