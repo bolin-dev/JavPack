9f3fb54974ec0e445abf4d2aa65562d537167133
bolinc <bolinchan2046@gmail.com>
2025-01-05 20:00:16 +0800

    修复数据提取逻辑中的潜在错误并优化性能

--- libs/JavPack.Req115.lib.js
+++ libs/JavPack.Req115.lib.js
@@ -1,7 +1,56 @@
 class Drive115 extends Req {
   static limit = 1150;
 
-  // Get file list
+  static filesSearch(search_value, params = {}) {
+    return this.request({
+      url: "https://webapi.115.com/files/search",
+      params: {
+        offset: 0,
+        limit: this.limit,
+        search_value,
+        date: "",
+        aid: 1,
+        cid: 0,
+        pick_code: "",
+        type: 0,
+        count_folders: 1,
+        source: "",
+        format: "json",
+        o: "user_ptime",
+        asc: 0,
+        star: "",
+        suffix: "",
+        ...params,
+      },
+      responseType: "json",
+    });
+  }
+
+  static filesAdd(cname, pid) {
+    return this.request({
+      method: "POST",
+      url: "https://webapi.115.com/files/add",
+      data: { cname, pid },
+    });
+  }
+
+  static lixianAddTaskUrl(url, wp_path_id) {
+    return this.request({
+      method: "POST",
+      url: "https://115.com/web/lixian/",
+      params: { ct: "lixian", ac: "add_task_url" },
+      data: { url, wp_path_id },
+    });
+  }
+
+  static lixianTaskLists() {
+    return this.request({
+      url: "https://115.com/web/lixian/",
+      params: { ct: "lixian", ac: "task_lists" },
+      responseType: "json",
+    });
+  }
+
   static files(cid = "0", params = {}) {
     return this.request({
       url: "https://webapi.115.com/files",
@@ -34,7 +83,6 @@ class Drive115 extends Req {
     });
   }
 
-  // Get file list by file name
   static natsortFiles(cid = "0", params = {}) {
     return this.request({
       url: "https://aps.115.com/natsort/files.php",
@@ -67,38 +115,24 @@ class Drive115 extends Req {
     });
   }
 
-  // Search for files
-  static filesSearch(search_value, params = {}) {
+  /**
+   * Bulk delete offline tasks and source files
+   * @param {string[]} hash Array of info_hashes
+   */
+  static lixianTaskDel(hash) {
     return this.request({
-      url: "https://webapi.115.com/files/search",
-      params: {
-        offset: 0,
-        limit: this.limit,
-        search_value,
-        date: "",
-        aid: 1,
-        cid: 0,
-        pick_code: "",
-        type: 0,
-        count_folders: 1,
-        source: "",
-        format: "json",
-        o: "user_ptime",
-        asc: 0,
-        star: "",
-        suffix: "",
-        ...params,
-      },
-      responseType: "json",
+      method: "POST",
+      url: "https://115.com/web/lixian/",
+      params: { ct: "lixian", ac: "task_del" },
+      data: { hash, flag: 1 },
     });
   }
 
-  // Create a new folder
-  static filesAdd(cname, pid) {
+  static rbClean(password) {
     return this.request({
       method: "POST",
-      url: "https://webapi.115.com/files/add",
-      data: { cname, pid },
+      url: "https://webapi.115.com/rb/clean",
+      data: { password },
     });
   }
 
@@ -115,44 +149,11 @@ class Drive115 extends Req {
     });
   }
 
-  static rb() {
+  static labelList(params = {}) {
     return this.request({
-      url: "https://webapi.115.com/rb",
-      params: {
-        aid: 7,
-        cid: 0,
-        offset: 0,
-        limit: this.limit,
-        source: "",
-        format: "json",
-      },
+      url: "https://webapi.115.com/label/list",
+      params: { keyword: "", limit: this.limit, ...params },
       responseType: "json",
-    });
-  }
-
-  /**
-   * Bulk clean files
-   * @param {string} password
-   * @param {string[]} rid Array of deleted file IDs
-   * @returns
-   */
-  static rbClean(password, rid) {
-    return this.request({
-      method: "POST",
-      url: "https://webapi.115.com/rb/clean",
-      data: { password, rid },
-    });
-  }
-
-  /**
-   * Bulk rename files
-   * @param {object} files_new_name { [fid]: rename }
-   */
-  static filesBatchRename(files_new_name) {
-    return this.request({
-      method: "POST",
-      url: "https://webapi.115.com/files/batch_rename",
-      data: { files_new_name },
     });
   }
 
@@ -171,6 +172,18 @@ class Drive115 extends Req {
   }
 
   /**
+   * Bulk rename files
+   * @param {object} files_new_name { [fid]: rename }
+   */
+  static filesBatchRename(files_new_name) {
+    return this.request({
+      method: "POST",
+      url: "https://webapi.115.com/files/batch_rename",
+      data: { files_new_name },
+    });
+  }
+
+  /**
    * Batch move files
    * @param {string[]} fid Array of file IDs
    * @param {string} pid Destination folder ID
@@ -183,48 +196,6 @@ class Drive115 extends Req {
     });
   }
 
-  // Get offline task list
-  static lixianTaskLists() {
-    return this.request({
-      url: "https://115.com/web/lixian/",
-      params: { ct: "lixian", ac: "task_lists" },
-      responseType: "json",
-    });
-  }
-
-  // Add offline task
-  static lixianAddTaskUrl(url, wp_path_id) {
-    return this.request({
-      method: "POST",
-      url: "https://115.com/web/lixian/",
-      params: { ct: "lixian", ac: "add_task_url" },
-      data: { url, wp_path_id },
-    });
-  }
-
-  /**
-   * Bulk delete offline tasks and source files
-   * @param {string[]} hash Array of info_hashes
-   */
-  static lixianTaskDel(hash) {
-    return this.request({
-      method: "POST",
-      url: "https://115.com/web/lixian/",
-      params: { ct: "lixian", ac: "task_del" },
-      data: { hash, flag: 1 },
-    });
-  }
-
-  // Get tag list
-  static labelList(params = {}) {
-    return this.request({
-      url: "https://webapi.115.com/label/list",
-      params: { keyword: "", limit: this.limit, ...params },
-      responseType: "json",
-    });
-  }
-
-  // Get upload signature
   static sampleInitUpload({ filename, filesize, cid }) {
     return this.request({
       method: "POST",
@@ -233,7 +204,6 @@ class Drive115 extends Req {
     });
   }
 
-  // Upload file
   static upload({
     host: url,
     filename: name,
@@ -260,7 +230,14 @@ class Drive115 extends Req {
     });
   }
 
-  // Get video file information
+  static filesEdit(fid, fid_cover) {
+    return this.request({
+      method: "POST",
+      url: "https://webapi.115.com/files/edit",
+      data: { fid, fid_cover },
+    });
+  }
+
   static filesVideo(pickcode) {
     return this.request({
       url: "https://v.anxia.com/webapi/files/video",
@@ -271,53 +248,35 @@ class Drive115 extends Req {
 }
 
 class Req115 extends Drive115 {
-  static async natsortFilesAll(cid, params = {}) {
-    const res = await this.natsortFiles(cid, params);
-    const { count, data, page_size } = res;
-    return count > page_size && data.length ? this.natsortFiles(cid, { ...params, limit: count }) : res;
-  }
-
-  static async filesSearchAll(search_value, params = {}) {
+  static async filesSearchAllVideos(search_value) {
+    const params = { type: 4 };
     const res = await this.filesSearch(search_value, params);
-    const { count, data, page_size } = res;
+
+    const { count, page_size, data } = res;
     return count > page_size && data.length ? this.filesSearch(search_value, { ...params, limit: count }) : res;
   }
 
-  static async filesAll(cid, params = {}) {
-    const res = await this.files(cid, params);
-    const { count, data, page_size } = res;
-    if (res.errNo === 20130827) return this.natsortFilesAll(cid, params);
-    return count > page_size && data.length ? this.files(cid, { ...params, limit: count }) : res;
-  }
-
-  // Search for videos
-  static videosSearch(search_value) {
-    return this.filesSearchAll(search_value, { type: 4 });
-  }
-
-  // Get video list
-  static videos(cid) {
-    return this.filesAll(cid, { type: 4 });
-  }
-
-  // Get subrip list
-  static subrips(cid) {
-    return this.filesAll(cid, { suffix: "srt" });
-  }
-
-  // Get folder list
-  static folders(cid) {
-    return this.filesAll(cid).then((res) => {
-      if (res?.data.length) res.data = res.data.filter(({ pid }) => Boolean(pid));
-      return res;
+  static filesSearchFolders(search_value, cid) {
+    return this.filesSearch(search_value, {
+      cid,
+      max_size: "",
+      limit: 50,
+      show_dir: 1,
+      o: "",
+      nf: 1,
+      qid: 0,
+      natsort: 1,
+      type: "",
+      fc: 1,
     });
   }
 
   static async generateCid(routes) {
+    if (routes.length === 1 && /^\d+$/.test(routes[0])) return routes[0];
     let cid = "0";
 
     for (const route of routes) {
-      const { data } = await this.folders(cid);
+      const { data } = await this.filesSearchFolders(route, cid);
       let item = data.find(({ n }) => n === route);
       if (!item) item = await this.filesAdd(route, cid);
       cid = item?.cid;
@@ -331,6 +290,24 @@ class Req115 extends Drive115 {
     return new Promise((r) => {
       setTimeout(r, s * 1000);
     });
+  }
+
+  static async natsortFilesAll(cid, params = {}) {
+    const res = await this.natsortFiles(cid, params);
+    const { count, page_size, data } = res;
+    return count > page_size && data.length ? this.natsortFiles(cid, { ...params, limit: count }) : res;
+  }
+
+  static async filesAll(cid, params = {}) {
+    const res = await this.files(cid, params);
+    if (res.errNo === 20130827) return this.natsortFilesAll(cid, params);
+
+    const { count, page_size, data } = res;
+    return count > page_size && data.length ? this.files(cid, { ...params, limit: count }) : res;
+  }
+
+  static videosAll(cid) {
+    return this.filesAll(cid, { type: 4 });
   }
 
   static async verifyTask(info_hash, { regex, codes }, { max, filter }) {
@@ -351,7 +328,7 @@ class Req115 extends Drive115 {
 
     for (let index = 0; index < max; index++) {
       if (index) await this.sleep();
-      const { data } = await this.videos(file_id);
+      const { data } = await this.videosAll(file_id);
 
       videos = data.filter((item) => regex.test(item.n));
       if (videos.length) break;
@@ -362,7 +339,7 @@ class Req115 extends Drive115 {
       const task = tasks.find((task) => task.info_hash === info_hash);
 
       if (task.status === 2) {
-        const { data } = await this.videos(file_id);
+        const { data } = await this.videosAll(file_id);
         videos = data.filter((item) => codes.some((it) => item.n.includes(it)));
       }
     }
@@ -370,12 +347,29 @@ class Req115 extends Drive115 {
     return { videos: videos.filter(filter), file_id };
   }
 
-  static handleRename(files, cid, { rename, renameTxt, zh, crack, subs }) {
-    rename = rename.replaceAll("$zh", zh || subs.length ? renameTxt.zh : "");
+  static async handleTags(files, tags) {
+    const list = (await this.labelList())?.data.list;
+    if (!list?.length) return;
+
+    const labels = [];
+    tags.forEach((tag) => {
+      const item = list.find(({ name }) => name === tag);
+      if (item) labels.push(item.id);
+    });
+    if (!labels.length) return;
+
+    return this.filesBatchLabel(files.map(({ fid }) => fid).toString(), labels.toString());
+  }
+
+  static subripsAll(cid) {
+    return this.filesAll(cid, { suffix: "srt" });
+  }
+
+  static handleRename(files, cid, { rename, renameTxt, zh, crack }) {
+    rename = rename.replaceAll("$zh", zh ? renameTxt.zh : "");
     rename = rename.replaceAll("$crack", crack ? renameTxt.crack : "");
     rename = rename.trim();
 
-    files = [...files, ...subs];
     const renameObj = { [cid]: rename };
 
     if (files.length === 1) {
@@ -408,20 +402,6 @@ class Req115 extends Drive115 {
     return this.filesBatchRename(renameObj);
   }
 
-  static async handleTags(files, tags) {
-    const list = (await this.labelList())?.data.list;
-    if (!list?.length) return;
-
-    const labels = [];
-    tags.forEach((tag) => {
-      const item = list.find(({ name }) => name === tag);
-      if (item) labels.push(item.id);
-    });
-    if (!labels.length) return;
-
-    return this.filesBatchLabel(files.map(({ fid }) => fid).toString(), labels.toString());
-  }
-
   static async handleClean(files, cid) {
     await this.filesMove(
       files.map((file) => file.fid),
@@ -437,35 +417,29 @@ class Req115 extends Drive115 {
     if (rm_fids.length) return this.rbDelete(rm_fids, cid);
   }
 
-  static async handleUpload(url, cid, filename) {
+  static async handleCover(url, cid, filename) {
     const file = await this.request({ url, responseType: "blob" });
     if (!file) return file;
 
     const res = await this.sampleInitUpload({ cid, filename, filesize: file.size });
     if (!res?.host) return res;
 
-    return this.upload({ ...res, filename, file });
+    const { data } = await this.upload({ ...res, filename, file });
+    if (data?.file_id) return this.filesEdit(cid, data.file_id);
   }
 
-  static async rbCleanByCid(cid, pwd) {
-    const res = await this.rb();
-    if (!res?.data.length) return;
-
-    const rid = res.data.filter((item) => item.cid === cid).map((item) => item.id);
-    if (rid.length) this.rbClean(pwd, rid);
-  }
-
-  static async handleSmartOffline(options, magnets) {
-    const { dir, regex, codes, verifyOptions, code, rename, renameTxt, tags, clean, cleanPwd, cover } = options;
+  static async handleOffline(
+    { dir, regex, codes, verifyOptions, cleanPwd, code, rename, renameTxt, tags, clean, cover },
+    magnets,
+  ) {
+    const res = { status: "error", msg: `获取目录失败: ${dir.join("/")}` };
     const cid = await this.generateCid(dir);
-
-    if (!cid) return { status: "error", msg: `获取目录失败: ${dir.join("/")}` };
-    const res = { msg: "", status: "" };
+    if (!cid) return res;
 
     for (let index = 0, { length } = magnets; index < length; index++) {
       const { url, zh, crack } = magnets[index];
 
-      const { state, errcode, error_msg, info_hash } = await this.lixianAddTaskUrl(url, cid);
+      const { state, error_msg, errcode, info_hash } = await this.lixianAddTaskUrl(url, cid);
       if (!state) {
         res.msg = error_msg;
         res.status = "error";
@@ -475,23 +449,27 @@ class Req115 extends Drive115 {
         break;
       }
 
-      const { file_id, videos } = await this.verifyTask(info_hash, { regex, codes }, verifyOptions);
+      const { videos, file_id } = await this.verifyTask(info_hash, { regex, codes }, verifyOptions);
       if (!videos.length) {
         if (verifyOptions.clean) {
-          this.lixianTaskDel([info_hash]);
+          this.lixianTaskDel([info_hash]).then(() => cleanPwd && this.rbClean(cleanPwd));
           if (file_id) this.rbDelete([file_id], cid);
         }
-        res.msg = `${code} 离线任务失败`;
+        res.msg = `${code} 离线验证失败`;
         res.status = "error";
         continue;
       }
 
-      const { data: subs } = await this.subrips(file_id);
-      if (rename) this.handleRename(videos, file_id, { rename, renameTxt, zh, crack, subs });
       if (tags.length) this.handleTags(videos, tags);
-      if (clean) await this.handleClean([...videos, ...subs], file_id);
-      if (cover) await this.handleUpload(cover, file_id, `${code}.cover.jpg`);
-      if (cleanPwd && clean) this.rbCleanByCid(file_id, cleanPwd);
+      const { data: subs } = await this.subripsAll(file_id);
+      const files = [...videos, ...subs];
+
+      if (rename) this.handleRename(files, file_id, { rename, renameTxt, zh: zh || subs.length, crack });
+      if (clean) {
+        await this.handleClean(files, file_id);
+        if (cleanPwd) this.rbClean(cleanPwd);
+      }
+      if (cover) await this.handleCover(cover, file_id, `${code}.cover.jpg`);
 
       res.msg = `${code} 离线任务成功`;
       res.status = "success";
