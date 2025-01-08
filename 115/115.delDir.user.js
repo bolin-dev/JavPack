1babd61966f0ccd94a7c6e84e90d6a8110d16880
bolinc <bolinchan2046@gmail.com>
2025-01-09 06:04:59 +0800

    fix: 优化删除逻辑，重构代码以提升可读性并添加观察者模式

--- 115/115.delDir.user.js
+++ 115/115.delDir.user.js
@@ -13,41 +13,53 @@
 // @run-at          document-end
 // @grant           GM_xmlhttpRequest
 // @grant           window.close
-// @grant           GM_addStyle
+// @require         https://github.com/Tampermonkey/utils/raw/d8a4543a5f828dfa8eefb0a3360859b6fe9c3c34/requires/gh_2215_make_GM_xhr_more_parallel_again.js
 // ==/UserScript==
 
 (function () {
+  const CONTAINER = document.querySelector(".vt-headline");
+  if (!CONTAINER) return;
+
   const smartDel = async ({ target }) => {
-    const { searchParams } = new URL(location);
-    const pc = searchParams.get("pickcode");
-    if (!pc) return;
+    const pickcode = new URL(location).searchParams.get("pickcode");
+    if (!pickcode || target.style.pointerEvents === "none") return;
 
-    target.textContent = "请求中...";
     target.style.pointerEvents = "none";
+    target.textContent = "请求中...";
 
-    const { parent_id, file_id } = await Req115.filesVideo(pc);
+    const { parent_id, file_id } = await Req115.filesVideo(pickcode);
     const { data } = await Req115.videosAll(parent_id);
     await Req115.rbDelete([data.length === 1 ? parent_id : file_id]);
 
-    const playlistNode = document.querySelector("#js-video_list");
-    if (playlistNode.querySelectorAll("li").length === 1) return window.close();
+    const listNode = document.querySelector("#js-video_list");
+    if (listNode.querySelectorAll("li").length === 1) return window.close();
 
     target.textContent = "删除";
     target.style.pointerEvents = "auto";
 
-    const curr = playlistNode.querySelector(".hover");
-    const nearby = curr.nextElementSibling ?? curr.previousElementSibling;
+    const curr = listNode.querySelector("li.hover");
+    const near = curr.nextElementSibling ?? curr.previousElementSibling;
+
     curr.remove();
-    nearby.querySelector("a").click();
+    near.querySelector("a").click();
   };
 
-  GM_addStyle(".vt-headline .btn-opendir + div {display:none}");
-  const delNode = document.createElement("a");
-  delNode.textContent = "删除";
-  delNode.className = "btn-opendir";
-  delNode.href = "javascript:void(0);";
-  delNode.addEventListener("click", smartDel);
+  const obsCont = (node) => {
+    const observer = new MutationObserver((mutations, obs) => {
+      if (mutations[0].type !== "childList") return;
+      CONTAINER.querySelector(":scope > div")?.remove();
+      CONTAINER.appendChild(node);
+      obs.disconnect();
+    });
+    observer.observe(CONTAINER, { childList: true, attributes: false, characterData: false });
+  };
 
-  document.querySelector(".vt-headline")?.insertAdjacentElement("beforeend", delNode);
+  const delNode = document.createElement("a");
+  delNode.href = "javascript:void(0);";
+  delNode.className = "btn-opendir";
+  delNode.textContent = "删除";
+  obsCont(delNode);
+
+  delNode.addEventListener("click", smartDel);
   document.addEventListener("keyup", ({ code }) => code === "Delete" && delNode.click());
 })();
