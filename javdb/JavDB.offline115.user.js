6c9827faec4bc5c04b92f959d882fb0af1b705b4
bolinc <bolinchan2046@gmail.com>
2025-01-16 10:33:48 +0800

    fix: 添加 @noframes 注释以优化脚本运行环境

--- javdb/JavDB.offline115.user.js
+++ javdb/JavDB.offline115.user.js
@@ -35,6 +35,7 @@
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @grant           GM_info
+// @noframes
 // @require         https://github.com/Tampermonkey/utils/raw/d8a4543a5f828dfa8eefb0a3360859b6fe9c3c34/requires/gh_2215_make_GM_xhr_more_parallel_again.js
 // ==/UserScript==
 
