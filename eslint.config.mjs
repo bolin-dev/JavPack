2114d047c1708b9371de36bc79fa1e973f9aa5c0
bolinc <bolinchan2046@gmail.com>
2026-01-30 06:03:29 +0800

    feat: add globals package and update eslint configuration for improved linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
 import antfu from "@antfu/eslint-config";
+import globals from "globals";
 
 export default antfu(
   {
     lessOpinionated: true,
     node: false,
-    typescript: false,
     jsx: false,
     unicorn: false,
     test: false,
-    vue: false,
     toml: false,
     stylistic: {
       quotes: "double",
@@ -19,13 +18,34 @@ export default antfu(
   },
   {
     files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.greasemonkey,
+        Grant: "readonly",
+        Magnet: "readonly",
+        Offline: "readonly",
+        Req: "readonly",
+        Req115: "readonly",
+        ReqDB: "readonly",
+        ReqMagnet: "readonly",
+        ReqSprite: "readonly",
+        ReqTrailer: "readonly",
+        Util: "readonly",
+        Verify115: "readonly",
+      },
+    },
     rules: {
-      "no-unused-vars": "off",
-      "unused-imports/no-unused-vars": ["error", {
-        varsIgnorePattern: "^Req",
-        argsIgnorePattern: "^_",
-      }],
       "curly": ["error", "multi-line", "consistent"],
+      "no-template-curly-in-string": "off",
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^(?:Grant|Magnet|Offline|Req(?:115|DB|Magnet|Sprite|Trailer)?|Util|Verify115)$",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 );
